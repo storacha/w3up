104480f307aab466d4545ddd0164eb55c31ca8e5
Hugo Dias <hugomrdias@gmail.com>
2022-04-26 11:31:42 +0100

    chore: test setup

--- packages/api/test/signer.test.js
+++ packages/api/test/signer.test.js
@@ -1,7 +1,21 @@
 import assert from 'assert'
+import { Miniflare } from 'miniflare'
 
-describe('signer', function () {
-  it('should sign', function () {
-    assert.ok(true)
+describe('auth ap', function () {
+  /** @type {Miniflare} */
+  let mf
+  before(() => {
+    // Create a new Miniflare environment for each test
+    mf = new Miniflare({
+      // Autoload configuration from `.env`, `package.json` and `wrangler.toml`
+      envPath: true,
+      packagePath: true,
+      wranglerConfigPath: true,
+    })
+  })
+
+  it('should work', async function () {
+    const res = await mf.dispatchFetch('http://localhost:8787')
+    assert.deepEqual(await res.json(), { msg: 'hello world!' })
   })
 })
