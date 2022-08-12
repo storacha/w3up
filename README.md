a07463ac6794c5e67484871efbc863ca5b0ecc5c
ice.breaker <ice.breaker@trigram.co>
2022-08-12 16:32:31 -0500

    feat: First pass at client lib for w3up.

---
+++ README.md
@@ -0,0 +1,15 @@
+## W3up Client
+
+This is a client for w3up.
+
+Used by w3up-cli, for most usage, go check it out.
+
+## Basic Usage
+
+This is a client wrapper around ucanto and definitions for the w3up service,
+allowing you to easily access the "uploads service" and get information
+about your uploaded content.
+
+## API
+
+Docs for the [API](./API.md)
---
+++ src/store/identity/client.js
@@ -0,0 +1,36 @@
+import { Authority } from '@ucanto/authority'
+import * as CBOR from '@ucanto/transport/cbor'
+import * as HTTP from '@ucanto/transport/http'
+import * as Client from '@ucanto/client'
+import webfetch from 'cross-fetch'
+
+// TODO: patch
+import * as CAR from '../../patches/@ucanto/transport/car.js'
+
+export * from './capability.js'
+
+/**
+ * @param {object} options
+ * @param {API.DID} options.id
+ * @param {URL} options.url
+ * @param {string} [options.method]
+ * @param {HTTP.Fetcher} [options.fetch]
+ * @param {API.OutpboundTranpsortOptions} [options.transport]
+ * @returns {API.ConnectionView<{identity: API.Identity.Identity}>}
+ */
+export const connect = ({
+  id,
+  url,
+  transport = { encoder: CAR, decoder: CBOR },
+  fetch = webfetch,
+  method,
+}) =>
+  Client.connect({
+    id: Authority.parse(id),
+    ...transport,
+    channel: HTTP.open({
+      url,
+      fetch,
+      method,
+    }),
+  })
