823041d34894b4d02ec50f6229ad9e088f5f2d84
Hugo Dias <hugomrdias@gmail.com>
2022-08-25 18:31:17 +0100

    fix(access-api): test error in staging

--- packages/access-api/src/routes/version.js
+++ packages/access-api/src/routes/version.js
@@ -1,14 +1,15 @@
-import { JSONResponse } from '@web3-storage/worker-utils/response'
+// import { JSONResponse } from '@web3-storage/worker-utils/response'
 
 /**
  * @param {import('@web3-storage/worker-utils/router').ParsedRequest} event
  * @param {import('../bindings.js').RouteContext} env
  */
 export function version(event, env) {
-  return new JSONResponse({
-    version: env.config.VERSION,
-    commit: env.config.COMMITHASH,
-    branch: env.config.BRANCH,
-    did: env.keypair.did(),
-  })
+  throw new Error('ipppp')
+  // return new JSONResponse({
+  //   version: env.config.VERSION,
+  //   commit: env.config.COMMITHASH,
+  //   branch: env.config.BRANCH,
+  //   did: env.keypair.did(),
+  // })
 }
