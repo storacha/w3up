bc73755a308d116ba42f67cef2c7a6be5307c327
Hugo Dias <hugomrdias@gmail.com>
2022-09-06 17:48:31 +0100

    feat: support ucan as bearer (#50)

--- packages/access-api/src/ucanto/server-codec.js
+++ packages/access-api/src/ucanto/server-codec.js
@@ -3,10 +3,73 @@ import * as UCAN from '@ipld/dag-ucan'
 import { UTF8 } from '@ucanto/transport'
 // eslint-disable-next-line no-unused-vars
 import * as Types from '@ucanto/interface'
+import { HTTPError } from '@web3-storage/worker-utils/error'
 
 const HEADERS = Object.freeze({
   'content-type': 'application/json',
 })
+
+/**
+ * Split multi value headers into an array
+ *
+ * @param {Headers} headers
+ * @param {string} name
+ */
+function multiValueHeader(headers, name) {
+  const out = headers
+    .get(name)
+    ?.split(',')
+    .map((v) => v.trimStart())
+
+  return out || []
+}
+
+/**
+ * @param {Record<string, string>} headers
+ */
+async function parseHeaders(headers) {
+  const h = new Headers(headers)
+
+  try {
+    /** @type Map<string,UCAN.Block> */
+    const proofs = new Map()
+    const proofsRaw = multiValueHeader(h, 'ucan')
+
+    for (const cidUcan of proofsRaw) {
+      const [cid, ucan] = cidUcan.trim().split(/\s+/)
+      const ucanView = UCAN.parse(/** @type {UCAN.JWT<any>} */ (ucan))
+      const block = await UCAN.write(ucanView)
+
+      if (cid !== block.cid.toString()) {
+        throw new TypeError(
+          `Invalid request, proof with cid ${block.cid.toString()} has mismatching cid ${cid} in the header`
+        )
+      }
+
+      proofs.set(cid, block)
+    }
+
+    /** @type {UCAN.View<any>[]} */
+    const ucans = []
+    const auths = multiValueHeader(h, 'Authorization')
+
+    for (const auth of auths) {
+      if (auth.toLowerCase().startsWith('bearer ')) {
+        const ucanView = UCAN.parse(
+          /** @type {UCAN.JWT<any>} */ (auth.slice(7))
+        )
+        ucans.push(ucanView)
+      }
+    }
+
+    return { proofs, ucans }
+  } catch (error) {
+    throw new HTTPError('Malformed UCAN headers data.', {
+      status: 401,
+      cause: /** @type {Error} */ (error),
+    })
+  }
+}
 
 /** @type {import('./types.js').ServerCodec} */
 export const serverCodec = {
@@ -17,29 +80,45 @@ export const serverCodec = {
    * @param {Types.HTTPRequest<I>} request
    */
   async decode({ body, headers }) {
-    const bearer = headers.authorization || ''
-    if (!bearer.toLowerCase().startsWith('bearer ')) {
-      throw Object.assign(new Error('bearer missing.'), {
-        status: 400,
-      })
-    }
-
-    const jwt = bearer.slice(7)
-    const invocations = []
-    try {
-      const data = UCAN.parse(/** @type {UCAN.JWT<any>} */ (jwt))
-      const root = await UCAN.write(data)
-
-      invocations.push(Delegation.create({ root }))
-      return /** @type {Types.InferInvocations<I>} */ (invocations)
-    } catch (error) {
-      throw Object.assign(
-        new Error('Invalid JWT.', { cause: /** @type {Error} */ (error) }),
-        {
-          status: 400,
-        }
+    const headersData = await parseHeaders(headers)
+    if (headersData.ucans.length === 0) {
+      throw new HTTPError(
+        'The required "Authorization: Bearer" header is missing.',
+        { status: 400 }
       )
     }
+    const invocations = []
+
+    // Iterate ucan invocations from the headers
+    for (const ucanView of headersData.ucans) {
+      const blocks = new Map()
+      const missing = []
+
+      // Check all the proofs for each invocation
+      for (const proofCID of ucanView.proofs) {
+        const proof = headersData.proofs.get(proofCID.toString())
+        if (!proof) {
+          missing.push(proofCID.toString())
+        }
+
+        blocks.set(proofCID.toString(), proof)
+        // TODO implement caching of proofs https://github.com/ucan-wg/ucan-as-bearer-token#32-cache-and-expiry
+      }
+
+      if (missing.length > 0) {
+        throw new HTTPError('Missing Proofs', {
+          status: 510,
+          // @ts-ignore - cause type is a mess
+          cause: { prf: missing },
+        })
+      }
+
+      // Build the full ucan chain for each invocation from headers data
+      invocations.push(
+        Delegation.create({ root: await UCAN.write(ucanView), blocks })
+      )
+    }
+    return /** @type {Types.InferInvocations<I>} */ (invocations)
   },
 
   /**
