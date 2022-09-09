38a5e776a2069493a30917f9872a1f1b1545ba11
ice.breaker <ice.breaker@trigram.co>
2022-09-09 11:43:10 -0500

    fix: update api docs, change build to build api docs / types.

--- types/defaults.d.ts
+++ types/defaults.d.ts
@@ -1,3 +1,3 @@
-declare const wssInightsUrl: "wss://bur1whjtc7.execute-api.us-east-1.amazonaws.com/staging";
-declare const insightsAPI: "https://rwj50bhvk9.execute-api.us-east-1.amazonaws.com";
+export const wssInightsUrl: "wss://bur1whjtc7.execute-api.us-east-1.amazonaws.com/staging";
+export const insightsAPI: "https://rwj50bhvk9.execute-api.us-east-1.amazonaws.com";
 //# sourceMappingURL=defaults.d.ts.map
