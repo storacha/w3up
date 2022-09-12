34579b069b5a03dec94be916ca487aabbe0d9075
Alan Shaw <alan.shaw@protocol.ai>
2022-09-12 16:21:05 +0100

    fix: temporary patch to allow fetch usage in browser
    Temp until #55 comes along

--- packages/access/src/connection.js
+++ packages/access/src/connection.js
@@ -9,7 +9,7 @@ import * as Service from './service.js'
  * @param {{id : import('@ipld/dag-ucan').Identity; url?: URL, fetch?: import('@ucanto/transport').HTTP.Fetcher}} opts
  * @returns {import('@ucanto/interface').ConnectionView<import('./types').Service>}
  */
-export function connection({ id, url = Service.url, fetch = _fetch }) {
+export function connection({ id, url = Service.url, fetch = _fetch.bind(globalThis) }) {
   return Client.connect({
     id,
     encoder: CAR,
