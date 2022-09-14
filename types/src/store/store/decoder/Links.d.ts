e618a34829c5bb3931f95be4c2e66b01c6c4757c
ice.breaker <ice.breaker@trigram.co>
2022-09-14 12:19:23 -0500

    fix: For now, keep built types in repo.

---
+++ types/src/store/store/decoder/Links.d.ts
@@ -0,0 +1,20 @@
+export function decode(input: unknown, options?: LinkOptions | undefined): Array<Link> | API.Failure;
+export function match(options: LinkOptions): API.Decoder<unknown, Array<Link | null>, API.Failure>;
+export function optional(options?: LinkOptions | undefined): API.Decoder<unknown, Array<Link> | undefined, API.Failure>;
+export type Code = number;
+export type Alg = number;
+export type Version = 1 | 0;
+export type LinkOptions = {
+    code?: Code;
+    algorithm?: Alg;
+    version?: Version;
+};
+export type Link = API.Link<unknown, Code, Alg, Version>;
+import { create } from "@ucanto/core/link";
+import { createV0 } from "@ucanto/core/link";
+import { isLink } from "@ucanto/core/link";
+import { asLink } from "@ucanto/core/link";
+import { parse } from "@ucanto/core/link";
+import * as API from "@ucanto/interface";
+import { Failure } from "@ucanto/validator";
+export { create, createV0, isLink, asLink, parse };
