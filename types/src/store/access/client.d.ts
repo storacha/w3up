e73b9bafa781049a444933aa6e98eb36b1a46ce7
ice.breaker <ice.breaker@trigram.co>
2022-09-15 18:23:05 -0500

    chore: update api and types.

--- types/src/store/access/client.d.ts
+++ types/src/store/access/client.d.ts
@@ -1,16 +1,9 @@
-import * as Client from '@ucanto/client'
-import * as API from '@ucanto/interface'
-
-export function connect({
-  id,
-  url,
-  transport,
-  fetch,
-  method,
-}: {
-  id: API.DID
-  url: URL
-  method?: string | undefined
-  fetch?: any
-  transport?: Client.OutpboundTranpsortOptions | undefined
-}): Client.ConnectionView<any>
+export function connect({ id, url, transport, fetch, method, }: {
+    id: API.DID;
+    url: URL;
+    method?: string | undefined;
+    fetch?: any;
+    transport?: Client.OutpboundTranpsortOptions | undefined;
+}): Client.ConnectionView<any>;
+import * as API from "@ucanto/interface";
+import * as Client from "@ucanto/client";
--- types/src/store/validation.d.ts
+++ types/src/store/validation.d.ts
@@ -1,11 +1,4 @@
-import * as API from '@ucanto/interface'
-import { Failure } from '@ucanto/validator'
-
-export function equalWith<
-  T extends API.ParsedCapability<API.UCAN.Ability, API.URI<`${string}:`>, any>,
-  U extends API.ParsedCapability<API.UCAN.Ability, API.URI<`${string}:`>, any>
->(claimed: T, delegated: U): true | Failure
-export function derivesURIPattern(
-  claimed: string,
-  delegated: string
-): true | Failure
+export function equalWith<T extends API.ParsedCapability<API.UCAN.Ability, API.URI<`${string}:`>, any>, U extends API.ParsedCapability<API.UCAN.Ability, API.URI<`${string}:`>, any>>(claimed: T, delegated: U): true | Failure;
+export function derivesURIPattern(claimed: string, delegated: string): true | Failure;
+import * as API from "@ucanto/interface";
+import { Failure } from "@ucanto/validator";
