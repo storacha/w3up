f14f3b223e8a7bd3f1f633ff7b1b2dac77c06471
ice.breaker <ice.breaker@trigram.co>
2022-09-23 00:34:30 -0500

    refactor: move types to dist only.

--- types/patches/@ucanto/transport/car.d.ts
+++
@@ -1,5 +0,0 @@
-export { CAR as codec };
-export function encode<I extends API.Transport.Tuple<API.IssuedInvocation<API.UCAN.Capability<API.UCAN.Ability, `${string}:${string}`>>>>(invocations: I, options?: API.EncodeOptions | undefined): Promise<API.HTTPRequest<I>>;
-export function decode<Invocations extends API.Transport.Tuple<API.IssuedInvocation<API.UCAN.Capability<API.UCAN.Ability, `${string}:${string}`>>>>({ headers, body }: API.HTTPRequest<Invocations>): Promise<API.InferInvocations<Invocations>>;
-import * as CAR from "./car/codec.js";
-import * as API from "@ucanto/interface";
--- types/src/store/validation.d.ts
+++
@@ -1,4 +0,0 @@
-export function equalWith<T extends API.ParsedCapability<API.UCAN.Ability, API.URI<`${string}:`>, any>, U extends API.ParsedCapability<API.UCAN.Ability, API.URI<`${string}:`>, any>>(claimed: T, delegated: U): true | Failure;
-export function derivesURIPattern(claimed: string, delegated: string): true | Failure;
-import * as API from "@ucanto/interface";
-import { Failure } from "@ucanto/validator";
