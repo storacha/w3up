33a333f5af0a034610962e4fbbcbe97f4a7d9a7e
Irakli Gozalishvili <contact@gozala.io>
2022-09-30 15:20:16 -0700

    feat: upload/* capabilities

--- packages/access/src/capabilities/types.ts
+++ packages/access/src/capabilities/types.ts
@@ -1,4 +1,6 @@
-import type { Capability, IPLDLink, DID } from '@ipld/dag-ucan'
+import type { Capability, IPLDLink, DID, ToString } from '@ipld/dag-ucan'
+import type { Block as IPLDBlock } from '@ucanto/interface'
+import { code as CAR_CODE } from '@ucanto/transport/car/codec'
 
 type AccountDID = DID
 type AgentDID = DID
@@ -56,3 +58,48 @@ export interface StoreRemove extends Capability<'store/remove', DID> {
 }
 
 export interface StoreList extends Capability<'store/list', DID> {}
+
+/**
+ * Logical represenatation of the CAR.
+ */
+export interface CAR {
+  roots: IPLDLink[]
+  blocks: Map<ToString<IPLDLink>, IPLDBlock>
+}
+
+export type CARLink = IPLDLink<CAR, typeof CAR_CODE>
+
+/**
+ * Capability to add arbitrary CID into an account's upload listing.
+ */
+export interface UploadAdd extends Capability<'upload/add', AccountDID> {
+  /**
+   * CID of the file / directory / DAG root that is uploaded.
+   */
+  root: IPLDLink
+  /**
+   * List of CAR links which MAY contain contents of this upload. Please
+   * note that there is no guarantee that linked CARs actually contain
+   * content related to this upload, it is whatever user deemed semantically
+   * relevant.
+   */
+  shards: CARLink[]
+}
+
+/**
+ * Capability to list CIDs in the account's upload list.
+ */
+export interface UploadList extends Capability<'upload/list', AccountDID> {
+  // ⚠️ We will likely add more fields here to support paging etc... but that
+  // will come in the future.
+}
+
+/**
+ * Capability to remove arbitrary CID from the account's upload list.
+ */
+export interface UploadRemove extends Capability<'upload/remove', AccountDID> {
+  /**
+   * CID of the file / directory / DAG root to be removed from the upload list.
+   */
+  root: IPLDLink
+}
