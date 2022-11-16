5daf40766a90d3e76338ab084349002b44f93120
Alan Shaw <alan.shaw@protocol.ai>
2022-11-16 13:03:59 +0000

    Update packages/upload-client/test/store.test.js
    Co-authored-by: Oli Evans <oli.evans@gmail.com>

--- packages/upload-client/test/store.test.js
+++ packages/upload-client/test/store.test.js
@@ -67,7 +67,7 @@ describe('Store.add', () => {
     const res = {
       status: 'upload',
       headers: { 'x-test': 'true' },
-      url: 'http://localhost:9400',
+      url: 'http://localhost:9400', // this bucket always returns a 400
     }
 
     const account = await Signer.generate()
