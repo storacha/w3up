3df3c300e660adfe1815dd9c1d425850d97a8cee
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-11-29 17:18:32 -0800

    Revert "remove duplicate test helpers across packges/access-client and packages/capabilities"
    This reverts commit 285c17759ad451164d0f64b0037894ad6c88d0a9.

--- packages/access-client/test/helpers/utils.js
+++ packages/access-client/test/helpers/utils.js
@@ -1,12 +1,35 @@
 // eslint-disable-next-line no-unused-vars
 import * as Ucanto from '@ucanto/interface'
+import { parseLink } from '@ucanto/core'
 import * as Server from '@ucanto/server'
 import * as Space from '@web3-storage/capabilities/space'
 import * as CAR from '@ucanto/transport/car'
 import * as CBOR from '@ucanto/transport/cbor'
 import { service } from './fixtures.js'
 
-export { createCarCid } from '@web3-storage/capabilities/test/helpers/utils.js'
+/**
+ * @param {string} source
+ */
+export function parseCarLink(source) {
+  return /** @type {Ucanto.Link<unknown, 514, number, 1>} */ (parseLink(source))
+}
+
+/**
+ * @param {any} data
+ */
+export async function createCborCid(data) {
+  const cbor = await CBOR.codec.write(data)
+  return cbor.cid
+}
+
+/**
+ * @param {string} source
+ */
+export async function createCarCid(source) {
+  const cbor = await CBOR.codec.write({ hello: source })
+  const shard = await CAR.codec.write({ roots: [cbor] })
+  return shard.cid
+}
 
 /**
  *
