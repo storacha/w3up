9c4f9267c45dc702554cb9e53b78dccf56ba6f7a
Alan Shaw <alan.shaw@protocol.ai>
2022-12-01 17:18:58 +0000

    fix: conf store

--- packages/access-client/src/stores/store-conf.js
+++ packages/access-client/src/stores/store-conf.js
@@ -1,24 +1,9 @@
-/* eslint-disable unicorn/no-null */
-/* eslint-disable jsdoc/check-indentation */
 import Conf from 'conf'
-import { Signer } from '@ucanto/principal/ed25519'
-import { delegationToString, stringToDelegation } from '../encoding.js'
-// eslint-disable-next-line no-unused-vars
-import * as Ucanto from '@ucanto/interface'
+import * as JSON from '../utils/json.js'
 
 /**
- * @typedef {import('../types').AgentData<Signer.EdSigner>} StoreData
- * @typedef {import('./types').IStore<Signer.EdSigner>} Store
- * @typedef {{
- *    meta: import('../types.js').AgentMeta
- *    principal: string
- *    currentSpace?: Ucanto.DID
- *    spaces: Array<[Ucanto.DID, import('../types').SpaceMeta]>
- *    delegations: Array<[import('../types').CIDString, {
- *      meta: import('../types').DelegationMeta,
- *      delegation: import('../types.js').EncodedDelegation
- *    }]>
- * }} Data
+ * @template T
+ * @typedef {import('./types').IStore<T>} Store
  */
 
 /**
@@ -30,31 +15,31 @@ import * as Ucanto from '@ucanto/interface'
  * import { StoreConf } from '@web3-storage/access/stores/store-conf'
  * ```
  *
- * @implements {Store}
+ * @template {Record<string, any>} T
+ * @implements {Store<T>}
  */
 export class StoreConf {
   /**
-   * @type {Conf<Data>}
+   * @type {Conf<T>}
    */
   #config
+
   /**
-   *
-   * @param {{
-   * profile: string
-   * }} opts
+   * @param {{ profile: string }} opts
    */
   constructor(opts) {
     this.#config = new Conf({
       projectName: 'w3access',
       projectSuffix: '',
       configName: opts.profile,
+      serialize: (v) => JSON.stringify(v),
+      deserialize: (v) => JSON.parse(v),
     })
     this.path = this.#config.path
   }
 
   /**
-   *
-   * @returns {Promise<Store>}
+   * @returns {Promise<Store<T>>}
    */
   async open() {
     return this
@@ -66,79 +51,19 @@ export class StoreConf {
     this.#config.clear()
   }
 
-  async exists() {
-    return this.#config.has('meta') && this.#config.has('principal')
-  }
-
-  /** @type {Store['init']} */
-  async init(data) {
-    const principal = data.principal || (await Signer.generate())
-
-    /** @type {StoreData} */
-    const storeData = {
-      meta: data.meta || { name: 'agent', type: 'device' },
-      spaces: data.spaces || new Map(),
-      delegations: data.delegations || new Map(),
-      principal,
-      currentSpace: data.currentSpace,
-    }
-
-    await this.save(storeData)
-    return storeData
-  }
-
   /**
-   *
-   * @param {StoreData} data
-   * @returns {Promise<Store>}
+   * @param {T} data
+   * @returns {Promise<Store<T>>}
    */
   async save(data) {
-    /** @type {Data['delegations']} */
-    const dels = []
-
-    for (const [key, value] of data.delegations) {
-      dels.push([
-        key,
-        {
-          meta: value.meta,
-          delegation: await delegationToString(value.delegation),
-        },
-      ])
-    }
-    /** @type {Data} */
-    const encodedData = {
-      currentSpace: data.currentSpace || undefined,
-      spaces: [...data.spaces.entries()],
-      meta: data.meta,
-      principal: Signer.format(data.principal),
-      delegations: dels,
-    }
-
-    this.#config.set(encodedData)
-
+    this.#config.set(data)
     return this
   }
 
-  /** @type {Store['load']} */
+  /** @returns {Promise<T|undefined>} */
   async load() {
-    const data = this.#config.store
-
-    /** @type {StoreData['delegations']} */
-    const dels = new Map()
-
-    for (const [key, value] of data.delegations) {
-      dels.set(key, {
-        delegation: await stringToDelegation(value.delegation),
-        meta: value.meta,
-      })
-    }
-    /** @type {StoreData} */
-    return {
-      principal: Signer.parse(data.principal),
-      currentSpace: data.currentSpace === null ? undefined : data.currentSpace,
-      meta: data.meta,
-      spaces: new Map(data.spaces),
-      delegations: dels,
-    }
+    const data = this.#config.store ?? {}
+    if (Object.keys(data).length === 0) return
+    return data
   }
 }
