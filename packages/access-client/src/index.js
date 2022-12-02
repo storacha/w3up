d5d3f853a66d91490c402e7e94f79d673f6f16d3
Hugo Dias <hugomrdias@gmail.com>
2022-12-02 12:09:39 +0000

    feat!: remove caps export from access-client

--- packages/access-client/src/index.js
+++ packages/access-client/src/index.js
@@ -1,8 +1,6 @@
 /* eslint-disable jsdoc/check-tag-names */
 export * from './agent.js'
 
-export { Space, Store, Top, Upload, Voucher } from '@web3-storage/capabilities'
-
 // Workaround for typedoc until 0.24 support export maps
 export * as Encoding from './encoding.js'
 export { StoreConf } from './stores/store-conf.js'
