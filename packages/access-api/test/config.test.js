a4785ddba774927fd00a988c5f2636a3ac5f887c
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-12-08 15:04:47 -0800

    access-api add test ensuring error when non-did config.DID provided to configureSigner

--- packages/access-api/test/config.test.js
+++ packages/access-api/test/config.test.js
@@ -1,28 +1,47 @@
 import assert from 'assert'
 import * as configModule from '../src/config.js'
 
+/** keypair that can be used for testing */
+const testKeypair = {
+  private: {
+    /**
+     * Private key encoded as multiformats
+     */
+    multiformats:
+      'MgCYWjE6vp0cn3amPan2xPO+f6EZ3I+KwuN1w2vx57vpJ9O0Bn4ci4jn8itwc121ujm7lDHkCW24LuKfZwIdmsifVysY=',
+  },
+  public: {
+    /**
+     * Public key encoded as a did:key
+     */
+    did: 'did:key:z6MkqBzPG7oNu7At8fktasQuS7QR7Tj7CujaijPMAgzdmAxD',
+  },
+}
+
 describe('@web3-storage/access-api/src/config configureSigner', () => {
-  it('configureSigner creates a signer using config.{DID,PRIVATE_KEY}', async () => {
+  it('creates a signer using config.{DID,PRIVATE_KEY}', async () => {
     const config = {
-      PRIVATE_KEY:
-        'MgCYWjE6vp0cn3amPan2xPO+f6EZ3I+KwuN1w2vx57vpJ9O0Bn4ci4jn8itwc121ujm7lDHkCW24LuKfZwIdmsifVysY=',
-      DID: 'did:web:example.com',
+      PRIVATE_KEY: testKeypair.private.multiformats,
+      DID: testKeypair.public.did,
     }
     const signer = configModule.configureSigner(config)
     assert.ok(signer)
     assert.equal(signer.did().toString(), config.DID)
   })
-  it('configureSigner infers did from config.PRIVATE_KEY when config.DID is omitted', async () => {
-    const testKey = {
-      PRIVATE_KEY:
-        'MgCYWjE6vp0cn3amPan2xPO+f6EZ3I+KwuN1w2vx57vpJ9O0Bn4ci4jn8itwc121ujm7lDHkCW24LuKfZwIdmsifVysY=',
-      didKey: 'did:key:z6MkqBzPG7oNu7At8fktasQuS7QR7Tj7CujaijPMAgzdmAxD',
-    }
+  it('errors if config.DID is provided but not a did', () => {
+    assert.throws(() => {
+      configModule.configureSigner({
+        DID: 'not a did',
+        PRIVATE_KEY: testKeypair.private.multiformats,
+      })
+    }, 'Invalid DID')
+  })
+  it('infers did from config.PRIVATE_KEY when config.DID is omitted', async () => {
     const config = {
-      PRIVATE_KEY: testKey.PRIVATE_KEY,
+      PRIVATE_KEY: testKeypair.private.multiformats,
     }
     const signer = configModule.configureSigner(config)
     assert.ok(signer)
-    assert.equal(signer.did().toString(), testKey.didKey)
+    assert.equal(signer.did().toString(), testKeypair.public.did)
   })
 })
