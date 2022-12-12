311da786b8f2fdf5ac585170e746a5a7b04ec57b
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-12-12 10:21:18 -0800

    feat: access-api uses DID env variable when building its ucanto server id (#275)
    Motivation:
    * https://github.com/web3-storage/w3protocol/issues/237
    
    Notes
    * This is intended to be safe to merge/deploy as is. The new
    functionality can be opted-into via env vars
    * The ucanto server will only be constructed with an opts.id that is a
    signer `withDID(...)` iff `process.env.DID` is set to a DID
    * If `process.env.DID` is unset, the ucanto server id signer will have a
    `did()` corresponding to `config.PRIVATE_KEY` (it will be a `did:key` of
    the corresponding public key)
    * Before this PR, many tests of the access-api worker were configured
    implicitly via dotenv with whatever the `/.env.local` file happened to
    contain. I added a worker test that asserts functionality that is
    dependent on the details of the env vars, so I made an affordance for
    [tests to be able to configure the access-api worker with environment
    variables defined in the test case
    itself](https://github.com/web3-storage/w3protocol/pull/275/files#diff-e20ffc550d006747790e7b67da280446c1cd1d2d4f72ccb5df04f62cbb6423daR149).

--- packages/access-api/test/ucan.test.js
+++ packages/access-api/test/ucan.test.js
@@ -144,6 +144,29 @@ describe('ucan', function () {
     t.deepEqual(rsp, ['test pass'])
   })
 
+  test('should support ucan invoking to a did:web aud', async function () {
+    const serviceDidWeb = 'did:web:example.com'
+    const { mf, issuer, service } = await context({
+      environment: {
+        ...process.env,
+        DID: serviceDidWeb,
+      },
+    })
+    const ucan = await UCAN.issue({
+      issuer,
+      audience: service.withDID(serviceDidWeb),
+      capabilities: [{ can: 'testing/pass', with: 'mailto:admin@dag.house' }],
+    })
+    const res = await mf.dispatchFetch('http://localhost:8787/raw', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${UCAN.format(ucan)}`,
+      },
+    })
+    const rsp = await res.json()
+    t.deepEqual(rsp, ['test pass'])
+  })
+
   test('should handle exception in route handler', async function () {
     const { mf, service, issuer } = ctx
 
