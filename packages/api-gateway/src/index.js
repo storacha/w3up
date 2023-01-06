52664d716401d5ab5c43bb3289b9385aa6e84437
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2023-01-05 17:57:36 -0800

    test voucher/claim proxy to production access

--- packages/api-gateway/src/index.js
+++ packages/api-gateway/src/index.js
@@ -3,6 +3,13 @@
 import { notFound } from '@web3-storage/worker-utils/response'
 import { Router } from '@web3-storage/worker-utils/router'
 import * as dagCbor from '@ipld/dag-cbor'
+import * as dagUcan from '@ipld/dag-ucan'
+import * as carTransport from '@ucanto/transport/car'
+import toBuffer from 'it-to-buffer'
+import toIt from 'browser-readablestream-to-it'
+// @ts-ignore
+// eslint-disable-next-line no-unused-vars
+import * as ucanto from '@ucanto/interface'
 // import { version } from './routes/version.js'
 
 /**
@@ -16,6 +23,99 @@ import * as dagCbor from '@ipld/dag-cbor'
 export function routeContextFromRequest(request) {
   return {
     url: new URL(request.url),
+  }
+}
+
+/**
+ * UCAN from http request authorization bearer token
+ */
+// @ts-ignore
+// eslint-disable-next-line no-unused-vars
+class UcanBearerToken {
+  /**
+   * @param {string} header - http authorization header value
+   * @returns {UcanBearerToken}
+   */
+  static fromHeader(header) {
+    return new UcanBearerToken(header.replace(/^Bearer /, ''))
+  }
+
+  /**
+   * @param {string} token
+   * @protected
+   */
+  constructor(token) {
+    if (!token) {
+      throw new Error(`cannot create UcanBearerToken from empty token`)
+    }
+    this.token = token
+    this.parsed = dagUcan.decode(Buffer.from(token))
+  }
+}
+
+/**
+ * @type {Record<string, (...args: any[]) => Promise<ucanto.Invocation>>}
+ */
+const UcantoInvocation = {
+  /**
+   * @param {Request} request
+   * @returns {Promise<ucanto.Invocation>}
+   */
+  fromRequest: async (request) => {
+    const invocations = await carTransport.decode({
+      headers: Object.fromEntries(request.headers.entries()),
+      body: request.body
+        ? await toBuffer(toIt(request.body))
+        : Uint8Array.from([]),
+    })
+    if (invocations.length !== 1) {
+      throw new Error(
+        `expected parsed request body CAR to contain exactly one invocation, got ${invocations.length}`
+      )
+    }
+    const [invocation] = invocations
+    return invocation
+  },
+}
+
+const SingleCapability = {
+  /**
+   * parse a single capability from a ucanto invocation.
+   * errors if invocation contains more than one capability.
+   *
+   * @template {dagUcan.Capability} C
+   * @param {ucanto.Invocation<C>} invocation
+   * @returns {C}
+   */
+  fromInvocation(invocation) {
+    if (invocation.capabilities.length !== 1) {
+      throw new Error(
+        `expected parsed invocation to contain exactly one capability, got ${invocation.capabilities.length}`
+      )
+    }
+    return invocation.capabilities[0]
+  },
+}
+
+/**
+ * @param {dagUcan.Capability} capability
+ * @returns {(req: Request) => Promise<Response>}
+ */
+function getUpstreamForCapability(capability) {
+  if (capability.can.startsWith('test/')) {
+    // respond with cbor
+    return async () => {
+      const body = dagCbor.encode([{}])
+      const headers = {
+        'content-type': 'application/cbor',
+      }
+      return new Response(body, { headers, status: 200 })
+    }
+  }
+  return async (request) => {
+    const upstreamRequest = new Request('https://access.web3.storage', request)
+    const response = await fetch(upstreamRequest)
+    return response
   }
 }
 
@@ -43,12 +143,17 @@ export class ApiGatewayWorker {
    * @type {ModuleWorker['fetch']}
    */
   fetchPostIndex = async (request, env, executionContext) => {
-    const status = 200
-    const headers = {
-      'content-type': 'application/cbor',
+    if (request.headers.get('content-type') !== 'application/car') {
+      return new Response('content-type must be application/car', {
+        status: 415,
+      })
     }
-    const body = dagCbor.encode({})
-    const response = new Response(body, { status, headers })
+    // get ucanto invocation from request
+    const invocation = await UcantoInvocation.fromRequest(request.clone())
+    const capability = SingleCapability.fromInvocation(invocation)
+    // determine upstream to handle capability
+    const upstreamFetch = getUpstreamForCapability(capability)
+    const response = await upstreamFetch(request.clone())
     return response
   }
 
