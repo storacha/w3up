57d0fa47a5e7f51c86688ca6bdc42b93f1562ea4
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2023-01-19 11:08:52 -0800

    feat: access-api proxy.js has configurable options.catchInvocationError, by default catches HTTPError -> error result w/ status=502 (#366)
    , which defaults to something that catches HTTPErrors and rewrites to a
    status=502 error result
    
    Motivation:
    * relates to: https://github.com/web3-storage/w3protocol/issues/363
    * instead of that behavior of getting a `HandlerExecutionError` (due to
    internally having an uncaught `HTTPError`), after this PR we should
    * not have the uncaught `HTTPError`, so no uncaught
    `HandlerExecutionError`
    * the result will have `status=502` and `x-proxy-error` with information
    about the proxy invocation request that errored (which will help debug
    #363)
    
    Limitations
    * This inlcudes in `result['x-proxy-error']` any info from the
    underlying `@ucanto/transport/http` `HTTPError`, but unless/until we put
    more info on that error, we still won't have the raw response object and
    e.g. won't have response headers.
    * but if those properties are ever added to `HTTPError`, they should
    show up in `x-proxy-error`

--- packages/access-api/src/ucanto/proxy.js
+++ packages/access-api/src/ucanto/proxy.js
@@ -2,9 +2,50 @@
 import * as Ucanto from '@ucanto/interface'
 import * as Client from '@ucanto/client'
 
+const BadGatewayHTTPErrorResult = {
+  /**
+   * Given unknown error, detect whether it is an upstream HTTPError.
+   * If so, return a result object indicating generic 'Bad Gateway'.
+   * Otherwise, if error is not a bad gateway error, return undefined
+   *
+   * @param {unknown} error - error encountered when proxying a ucanto invocation to an upstream url
+   */
+  catch(error) {
+    if (!error || typeof error !== 'object') {
+      return
+    }
+    const status = 'status' in error ? Number(error.status) : undefined
+    const isServerError = status !== undefined && status >= 500 && status < 600
+    if (!isServerError) {
+      return
+    }
+    return {
+      error: true,
+      status: 502,
+      statusText: 'Bad Gateway',
+      'x-proxy-error': error,
+    }
+  },
+}
+
+/**
+ * default catchInvocationError value for createProxyHandler.
+ * It catches `HTTPError` errors to an error result with status=502 and statusText='Bad Gateway'
+ *
+ * @param {unknown} error
+ */
+function defaultCatchInvocationError(error) {
+  const badGatewayResult = BadGatewayHTTPErrorResult.catch(error)
+  if (badGatewayResult) {
+    return badGatewayResult
+  }
+  throw error
+}
+
 /**
  * @template {Ucanto.ConnectionView<any>} [Connection=Ucanto.ConnectionView<any>]
  * @param {object} options
+ * @param {(error: unknown) => Promise<unknown>} [options.catchInvocationError] - catches any error that comes from invoking the proxy invocation on the connection. If it returns a value, that value will be the proxied invocation result.
  * @param {{ default: Connection, [K: Ucanto.UCAN.DID]: Connection }} options.connections
  * @param {Ucanto.Signer} [options.signer]
  */
@@ -16,8 +57,12 @@ export function createProxyHandler(options) {
    * @returns {Promise<Ucanto.Result<any, { error: true }>>}
    */
   return async function handleInvocation(invocationIn, context) {
-    const { connections, signer } = options
-    const { audience, capabilities } = invocationIn
+    const {
+      connections,
+      signer,
+      catchInvocationError = defaultCatchInvocationError,
+    } = options
+    const { audience, capabilities, expiration, notBefore } = invocationIn
     const connection = connections[audience.did()] ?? connections.default
     // eslint-disable-next-line unicorn/prefer-logical-operator-over-ternary, no-unneeded-ternary
     const proxyInvocationIssuer = signer
@@ -28,18 +73,26 @@ export function createProxyHandler(options) {
       : // this works, but involves lying about the issuer type (it wants a Signer but context.id is only a Verifier)
         // @todo obviate this type override via https://github.com/web3-storage/ucanto/issues/195
         /** @type {Ucanto.Signer} */ (context.id)
-
-    const [result] = await Client.execute(
-      [
-        Client.invoke({
-          issuer: proxyInvocationIssuer,
-          capability: capabilities[0],
-          audience,
-          proofs: [invocationIn],
-        }),
-      ],
-      /** @type {Client.ConnectionView<any>} */ (connection)
-    )
-    return result
+    const proxyInvocation = Client.invoke({
+      issuer: proxyInvocationIssuer,
+      capability: capabilities[0],
+      audience,
+      proofs: [invocationIn],
+      expiration,
+      notBefore,
+    })
+    try {
+      const [result] = await Client.execute(
+        [proxyInvocation],
+        /** @type {Client.ConnectionView<any>} */ (connection)
+      )
+      return result
+    } catch (error) {
+      if (catchInvocationError) {
+        const caughtResult = await catchInvocationError(error)
+        return caughtResult
+      }
+      throw error
+    }
   }
 }
