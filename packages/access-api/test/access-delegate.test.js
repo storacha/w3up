ed517e5e1a36665ace651aadef2c42c31232301f
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2023-02-08 16:29:29 -0800

    access-delegate.test.js tests invoking handler directly as well as invoking through miniflare

--- packages/access-api/test/access-delegate.test.js
+++ packages/access-api/test/access-delegate.test.js
@@ -7,73 +7,155 @@ import * as ucanto from '@ucanto/core'
 import * as principal from '@ucanto/principal'
 import { createAccessDelegateHandler } from '../src/service/access-delegate.js'
 
-describe('access-api handling access/delegate', function () {
-  for (const [variantName, createInvocation] of Object.entries(
-    namedDelegateVariants()
-  )) {
-    it(`handles invocation variant ${variantName}`, async () => {
-      const { service, conn, issuer } = await context()
-      const invocation = await createInvocation({ issuer, audience: service })
-      const [result] = await conn.execute(invocation)
-      assert.notDeepEqual(
-        result.error,
-        true,
-        'invocation result is not an error'
-      )
-    })
-  }
-})
+for (const tester of [
+  {
+    name: 'handled by access-api in miniflare',
+    ...createTesterFromContext(() => context()),
+  },
+  {
+    name: 'handled by access-delegate-handler',
+    ...createTesterFromHandler(() => createAccessDelegateHandler()),
+  },
+]) {
+  describe(`access/delegate ${tester.name}`, () => {
+    for (const [variantName, createTest] of Object.entries(
+      namedDelegateVariants()
+    )) {
+      it(`handles variant ${variantName}`, async () => {
+        const { issuer, audience, invoke } = tester
+        const { invocation, check } = await createTest({ issuer, audience })
+        /** @type {Ucanto.Result<unknown, { error: true }>} */
+        const result = await invoke(invocation)
+        if (typeof check === 'function') {
+          await check(result)
+        }
+        assert.notDeepEqual(
+          result.error,
+          true,
+          'invocation result is not an error'
+        )
+      })
+    }
+  })
+}
 
 /**
- * @typedef {(options: { issuer: Ucanto.Signer<Ucanto.DID<'key'>>, audience: Ucanto.Principal }) => Promise<Ucanto.Delegation<[Ucanto.InferInvokedCapability<typeof Access.delegate>]>>} AccessDelegateInvocationFactory
+ * Tests using context from "./helpers/context.js", which sets up a testable access-api inside miniflare.
+ *
+ * @param {() => Promise<{ issuer: Ucanto.Signer<Ucanto.DID<'key'>>, service: Ucanto.Signer<Ucanto.DID>, conn: Ucanto.ConnectionView<Record<string, any>> }>} createContext
+ */
+function createTesterFromContext(createContext) {
+  const context = createContext()
+  const issuer = context.then(({ issuer }) => issuer)
+  const audience = context.then(({ service }) => service)
+  /**
+   * @template {Ucanto.Capability} Capability
+   * @param {Ucanto.Invocation<Capability>} invocation
+   */
+  const invoke = async (invocation) => {
+    const { conn } = await context
+    const [result] = await conn.execute(invocation)
+    return result
+  }
+  return { issuer, audience, invoke }
+}
+
+/**
+ * Tests using simple function invocation -> result
+ *
+ * @template {Ucanto.Capability} Capability
+ * @template Result
+ * @param {() => (invocation: Ucanto.Invocation<Capability>) => Promise<Result>} createHandler
+ */
+function createTesterFromHandler(createHandler) {
+  const issuer = principal.ed25519.generate()
+  const audience = principal.ed25519.generate()
+  /**
+   * @param {Ucanto.Invocation<Capability>} invocation
+   */
+  const invoke = async (invocation) => {
+    const handle = createHandler()
+    const result = await handle(invocation)
+    return result
+  }
+  return { issuer, audience, invoke }
+}
+
+/**
+ * a value that can be passed to Promise.resolve() to get Promise<T>
+ *
+ * @template T
+ * @typedef {T | Promise<T>} Resolvable
+ */
+
+/**
+ * @typedef InvocationContext
+ * @property {Resolvable<Ucanto.Signer<Ucanto.DID<'key'>>>} issuer
+ * @property {Resolvable<Ucanto.Principal>} audience
+ */
+
+/**
+ * @template {Ucanto.CapabilityParser<Ucanto.Match<Ucanto.ParsedCapability>>} CapabilityParser
+ * @template [Success=unknown]
+ * @typedef InvocationTest
+ * @property {Ucanto.Invocation<Ucanto.InferInvokedCapability<CapabilityParser>>} invocation
+ * @property {(result: Ucanto.Result<Success, { error: true }>) => Resolvable<void>} [check] - check the result of the invocation. throw if not valid
+ */
+
+/**
+ * @template {Ucanto.CapabilityParser<Ucanto.Match<Ucanto.ParsedCapability>>} CapabilityParser
+ * @template [Success=unknown]
+ * @typedef {(options: InvocationContext) => Promise<InvocationTest<CapabilityParser, Success>>} InvocationTestCreator
  */
 
 /**
  * create valid delegate invocation with an empty delegation set
  *
- * @type {AccessDelegateInvocationFactory}
+ * @type {InvocationTestCreator<typeof Access.delegate>}
  */
-function withEmptyDelegationSet({ issuer, audience }) {
-  return Access.delegate
+async function withEmptyDelegationSet({ issuer, audience }) {
+  const invocation = await Access.delegate
     .invoke({
-      issuer,
-      audience,
-      with: issuer.did(),
+      issuer: await issuer,
+      audience: await audience,
+      with: await Promise.resolve(issuer).then((i) => i.did()),
       nb: {
         delegations: {},
       },
     })
     .delegate()
+  return { invocation }
 }
 
 /**
  * create a valid delegate invocation with a single delegation in nb.delegations set
  *
- * @type {AccessDelegateInvocationFactory}
+ * @type {InvocationTestCreator<typeof Access.delegate>}
  */
 async function withSingleDelegation({ issuer, audience }) {
-  return Access.delegate
+  const delegation = await ucanto.delegate({
+    issuer: await issuer,
+    audience: await audience,
+    capabilities: [{ can: '*', with: 'urn:foo' }],
+  })
+  const invocation = await Access.delegate
     .invoke({
-      issuer,
-      audience,
-      with: issuer.did(),
+      issuer: await issuer,
+      audience: await audience,
+      with: await Promise.resolve(issuer).then((i) => i.did()),
       nb: {
         delegations: {
-          notACid: await ucanto
-            .delegate({
-              issuer,
-              audience,
-              capabilities: [{ can: '*', with: 'urn:foo' }],
-            })
-            .then(({ cid }) => cid),
+          notACid: delegation.cid,
         },
       },
+      proofs: [delegation],
     })
     .delegate()
+  return { invocation }
 }
 
 /**
- * @returns {Record<string, AccessDelegateInvocationFactory>}
+ * @returns {Record<string, InvocationTestCreator<typeof Access.delegate>>}
  */
 function namedDelegateVariants() {
   return {
