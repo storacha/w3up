f36911dced259d366fb652ca1e96c8bb81ec4b3c
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2023-02-10 15:58:42 -0800

    wip encoding delegations in AccessClaim result

--- packages/access-api/test/delegations-storage.test.js
+++ packages/access-api/test/delegations-storage.test.js
@@ -1,10 +1,8 @@
 import { context } from './helpers/context.js'
 import { DbDelegationsStorage } from '../src/models/delegations.js'
-import * as principal from '@ucanto/principal'
-import * as Ucanto from '@ucanto/interface'
-import * as ucanto from '@ucanto/core'
 import { createD1Database } from '../src/utils/d1.js'
 import * as assert from 'node:assert'
+import { createSampleDelegation } from '../src/utils/ucan.js'
 
 describe('DbDelegationsStorage', () => {
   it('should persist delegations', async () => {
@@ -18,30 +16,3 @@ describe('DbDelegationsStorage', () => {
     assert.deepEqual(await storage.length, delegations.length)
   })
 })
-
-/**
- *
- * @param {object} options
- * @param {PromiseLike<principal.ed25519.EdSigner>} [options.audience]
- * @param {PromiseLike<principal.ed25519.EdSigner>} [options.issuer]
- * @param {Ucanto.URI} [options.with]
- * @param {Ucanto.Ability} [options.can]
- */
-async function createSampleDelegation(options = {}) {
-  const {
-    issuer = Promise.resolve(principal.ed25519.generate()),
-    audience = Promise.resolve(principal.ed25519.generate()),
-    can,
-  } = options
-  const delegation = await ucanto.delegate({
-    issuer: await issuer,
-    audience: await audience,
-    capabilities: [
-      {
-        with: options.with || 'urn:',
-        can: can || 'test/*',
-      },
-    ],
-  })
-  return delegation
-}
