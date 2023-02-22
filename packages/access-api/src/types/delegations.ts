421c46dcbc56a6f6d78b9e073725df1957a1df50
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2023-02-22 15:14:23 -0800

    document methods on DelegationsStorage

--- packages/access-api/src/types/delegations.ts
+++ packages/access-api/src/types/delegations.ts
@@ -5,10 +5,23 @@ export type Resolvable<T> = T | Promise<T>
 export interface DelegationsStorage<
   Cap extends Ucanto.Capability = Ucanto.Capability
 > {
+  /**
+   * push items into storage
+   *
+   * @param delegations - delegations to store
+   */
   push: (
-    ...delegation: Array<Ucanto.Delegation<Ucanto.Tuple<Cap>>>
+    ...delegations: Array<Ucanto.Delegation<Ucanto.Tuple<Cap>>>
   ) => Resolvable<unknown>
+
+  /**
+   * get number of stored items
+   */
   length: Resolvable<number>
+
+  /**
+   * iterate through all stored items
+   */
   [Symbol.asyncIterator]: () => AsyncIterableIterator<
     Ucanto.Delegation<Ucanto.Tuple<Cap>>
   >
