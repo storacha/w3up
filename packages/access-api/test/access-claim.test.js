2ec16e9cd3433cb7915cb5c8c107321ab64e699e
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2023-02-27 17:05:07 -0800

    feat: access-api serves access/claim invocations (#456)
    Motivation:
    * https://github.com/web3-storage/w3protocol/issues/455
    * this will definitely resolve this error
    https://github.com/gobengo/w3protocol-test/actions/runs/4267476381/jobs/7429121484#step:6:66
    * [ ] test that you can only claim delegations whose audience matches
    access/claim invocation `with`

---
+++ packages/access-api/test/access-claim.test.js
@@ -0,0 +1,45 @@
+import { context } from './helpers/context.js'
+import { createTesterFromContext } from './helpers/ucanto-test-utils.js'
+import { ed25519 } from '@ucanto/principal'
+import { claim } from '@web3-storage/capabilities/access'
+import * as assert from 'assert'
+
+/**
+ * Run the same tests against several variants of access/delegate handlers.
+ */
+for (const handlerVariant of /** @type {const} */ ([
+  {
+    name: 'handled by access-api in miniflare',
+    ...(() => {
+      const spaceWithStorageProvider = ed25519.generate()
+      return {
+        spaceWithStorageProvider,
+        ...createTesterFromContext(() => context(), {
+          registerSpaces: [spaceWithStorageProvider],
+        }),
+      }
+    })(),
+  },
+])) {
+  describe(`access-claim ${handlerVariant.name}`, () => {
+    it(`can be invoked`, async () => {
+      const issuer = await handlerVariant.issuer
+      const result = await handlerVariant.invoke(
+        await claim
+          .invoke({
+            issuer,
+            audience: await handlerVariant.audience,
+            with: issuer.did(),
+          })
+          .delegate()
+      )
+      assert.deepEqual(
+        'delegations' in result,
+        true,
+        'result contains delegations set'
+      )
+    })
+  })
+
+  // there are more tests about `testDelegateThenClaim` in ./access-delegate.test.js
+}
