cd7dc57b4dc37fd34ae7c8900f2e292d0c181d2f
Irakli Gozalishvili <contact@gozala.io>
2023-03-03 12:41:57 -0800

    fix: tests for provider add (#477)

--- packages/capabilities/test/capabilities/provider.test.js
+++ packages/capabilities/test/capabilities/provider.test.js
@@ -2,36 +2,24 @@ import assert from 'assert'
 import { access } from '@ucanto/validator'
 import { Verifier } from '@ucanto/principal/ed25519'
 import * as Provider from '../../src/provider.js'
-import { bob, service, mallory } from '../helpers/fixtures.js'
+import { bob, service, alice, mallory } from '../helpers/fixtures.js'
+import { createAuthorization } from '../helpers/utils.js'
 import * as ucanto from '@ucanto/core'
 
 describe('provider/add', function () {
-  it.skip('can invoke as account with ./update', async function () {
-    const account = mallory.withDID('did:mailto:mallory.com:mallory')
+  it('can invoke as an account', async function () {
+    const agent = alice
+    const account = 'did:mailto:mallory.com:mallory'
     const space = bob
     const auth = Provider.add.invoke({
-      issuer: account,
+      issuer: agent,
       audience: service,
-      with: account.did(),
+      with: account,
       nb: {
         provider: 'did:web:web3.storage:providers:w3up-alpha',
         consumer: space.did(),
       },
-      proofs: [
-        await ucanto.delegate({
-          issuer: service,
-          audience: account,
-          capabilities: [
-            {
-              with: service.did(),
-              can: './update',
-              nb: {
-                key: mallory.did(),
-              },
-            },
-          ],
-        }),
-      ],
+      proofs: await createAuthorization({ agent, service, account }),
     })
 
     const result = await access(await auth.delegate(), {
@@ -51,7 +39,88 @@ describe('provider/add', function () {
     }
   })
 
-  it('should not support undefined consumer', async function () {
+  it('fails without account delegation', async function () {
+    const agent = alice
+    const account = 'did:mailto:mallory.com:mallory'
+    const space = bob
+    const auth = Provider.add.invoke({
+      issuer: agent,
+      audience: service,
+      with: account,
+      nb: {
+        provider: 'did:web:web3.storage:providers:w3up-alpha',
+        consumer: space.did(),
+      },
+    })
+
+    const result = await access(await auth.delegate(), {
+      capability: Provider.add,
+      principal: Verifier,
+      authority: service,
+    })
+
+    assert.equal(result.toString().includes('not authorized'), true)
+  })
+
+  it('fails without attestation', async function () {
+    const agent = alice
+    const account = 'did:mailto:mallory.com:mallory'
+    const space = bob
+    const [delegation] = await createAuthorization({
+      agent,
+      service,
+      account,
+    })
+    const auth = Provider.add.invoke({
+      issuer: agent,
+      audience: service,
+      with: account,
+      nb: {
+        provider: 'did:web:web3.storage:providers:w3up-alpha',
+        consumer: space.did(),
+      },
+      proofs: [delegation],
+    })
+
+    const result = await access(await auth.delegate(), {
+      capability: Provider.add,
+      principal: Verifier,
+      authority: service,
+    })
+
+    assert.equal(result.toString().includes('not authorized'), true)
+  })
+
+  it('fails without account delegation', async function () {
+    const agent = alice
+    const account = 'did:mailto:mallory.com:mallory'
+    const space = bob
+    const [, attestation] = await createAuthorization({
+      agent,
+      service,
+      account,
+    })
+    const auth = Provider.add.invoke({
+      issuer: agent,
+      audience: service,
+      with: account,
+      nb: {
+        provider: 'did:web:web3.storage:providers:w3up-alpha',
+        consumer: space.did(),
+      },
+      proofs: [attestation],
+    })
+
+    const result = await access(await auth.delegate(), {
+      capability: Provider.add,
+      principal: Verifier,
+      authority: service,
+    })
+
+    assert.equal(result.toString().includes('not authorized'), true)
+  })
+
+  it('requires nb.consumer', async function () {
     const bobAccount = bob.withDID('did:mailto:bob.com:bob')
     assert.throws(() => {
       Provider.add.invoke({
@@ -64,5 +133,271 @@ describe('provider/add', function () {
         },
       })
     }, /Error: Invalid 'nb' - Object contains invalid field "consumer"/)
+  })
+
+  it('nb.consumer must be a did:key', async function () {
+    const bobAccount = bob.withDID('did:mailto:bob.com:bob')
+    assert.throws(() => {
+      Provider.add.invoke({
+        issuer: bob,
+        audience: service,
+        with: bobAccount.did(),
+        nb: {
+          provider: 'did:web:web3.storage:providers:w3up-alpha',
+          // @ts-expect-error
+          consumer: 'did:mailto:web3.storage:user',
+        },
+      })
+    }, /Error: Invalid 'nb' - Object contains invalid field "consumer"/)
+  })
+
+  it('requires nb.provider', async function () {
+    const bobAccount = bob.withDID('did:mailto:bob.com:bob')
+    assert.throws(() => {
+      Provider.add.invoke({
+        issuer: bob,
+        audience: service,
+        with: bobAccount.did(),
+        // @ts-expect-error - missing provider
+        nb: {
+          // provider: 'did:web:web3.storage:providers:w3up-alpha',
+          consumer: bob.did(),
+        },
+      })
+    }, /Error: Invalid 'nb' - Object contains invalid field "provider"/)
+  })
+
+  it('requires nb.provider be registered', async function () {
+    const bobAccount = bob.withDID('did:mailto:bob.com:bob')
+    assert.throws(() => {
+      Provider.add.invoke({
+        issuer: bob,
+        audience: service,
+        with: bobAccount.did(),
+        nb: {
+          // @ts-expect-error - not registered
+          provider: 'did:web:web3.storage:providers:w3up-beta',
+          consumer: bob.did(),
+        },
+      })
+    }, /Error: Invalid 'nb' - Object contains invalid field "provider"/)
+  })
+
+  it('can delegate provider/add', async () => {
+    const agent = alice
+    const account = 'did:mailto:mallory.com:mallory'
+    const space = mallory
+    const auth = Provider.add.invoke({
+      issuer: bob,
+      audience: service,
+      with: account,
+      nb: {
+        provider: 'did:web:web3.storage:providers:w3up-alpha',
+        consumer: space.did(),
+      },
+      proofs: [
+        await Provider.add.delegate({
+          issuer: agent,
+          audience: bob,
+          with: account,
+          nb: {
+            provider: 'did:web:web3.storage:providers:w3up-alpha',
+            consumer: space.did(),
+          },
+          proofs: await createAuthorization({ agent, service, account }),
+        }),
+      ],
+    })
+
+    const result = await access(await auth.delegate(), {
+      capability: Provider.add,
+      principal: Verifier,
+      authority: service,
+    })
+
+    assert.equal(result.error, undefined)
+  })
+
+  it('can delegate provider/add without setting consumer', async () => {
+    const agent = alice
+    const account = 'did:mailto:mallory.com:mallory'
+    const space = mallory
+    const auth = Provider.add.invoke({
+      issuer: bob,
+      audience: service,
+      with: account,
+      nb: {
+        provider: 'did:web:web3.storage:providers:w3up-alpha',
+        consumer: space.did(),
+      },
+      proofs: [
+        await Provider.add.delegate({
+          issuer: agent,
+          audience: bob,
+          with: account,
+          nb: {
+            provider: 'did:web:web3.storage:providers:w3up-alpha',
+          },
+          proofs: await createAuthorization({ agent, service, account }),
+        }),
+      ],
+    })
+
+    const result = await access(await auth.delegate(), {
+      capability: Provider.add,
+      principal: Verifier,
+      authority: service,
+    })
+
+    assert.equal(result.error, undefined)
+  })
+
+  it('can delegate provider/add without setting provider', async () => {
+    const agent = alice
+    const account = 'did:mailto:mallory.com:mallory'
+    const space = mallory
+    const auth = Provider.add.invoke({
+      issuer: bob,
+      audience: service,
+      with: account,
+      nb: {
+        provider: 'did:web:web3.storage:providers:w3up-alpha',
+        consumer: space.did(),
+      },
+      proofs: [
+        await Provider.add.delegate({
+          issuer: agent,
+          audience: bob,
+          with: account,
+          nb: {
+            consumer: space.did(),
+          },
+          proofs: await createAuthorization({ agent, service, account }),
+        }),
+      ],
+    })
+
+    const result = await access(await auth.delegate(), {
+      capability: Provider.add,
+      principal: Verifier,
+      authority: service,
+    })
+
+    assert.equal(result.error, undefined)
+  })
+
+  it('can not change delegated consumer', async () => {
+    const agent = alice
+    const account = 'did:mailto:mallory.com:mallory'
+    const space = mallory
+    const auth = Provider.add.invoke({
+      issuer: bob,
+      audience: service,
+      with: account,
+      nb: {
+        provider: 'did:web:web3.storage:providers:w3up-alpha',
+        consumer: bob.did(),
+      },
+      proofs: [
+        await Provider.add.delegate({
+          issuer: agent,
+          audience: bob,
+          with: account,
+          nb: {
+            consumer: space.did(),
+          },
+          proofs: await createAuthorization({ agent, service, account }),
+        }),
+      ],
+    })
+
+    const result = await access(await auth.delegate(), {
+      capability: Provider.add,
+      principal: Verifier,
+      authority: service,
+    })
+
+    assert.equal(result.toString().includes('Constrain violation'), true)
+    assert.equal(result.error, true)
+  })
+
+  it('can not change delegated provider', async () => {
+    const agent = alice
+    const account = 'did:mailto:mallory.com:mallory'
+    const space = mallory
+    const auth = Provider.add.invoke({
+      issuer: bob,
+      audience: service,
+      with: account,
+      nb: {
+        provider: 'did:web:web3.storage:providers:w3up-alpha',
+        consumer: bob.did(),
+      },
+      proofs: [
+        await ucanto.delegate({
+          issuer: agent,
+          audience: bob,
+          capabilities: [
+            {
+              with: account,
+              can: 'provider/add',
+              nb: {
+                provider: 'did:web:web3.storage:providers:w3up-beta',
+                consumer: space.did(),
+              },
+            },
+          ],
+          proofs: await createAuthorization({ agent, service, account }),
+        }),
+      ],
+    })
+
+    const result = await access(await auth.delegate(), {
+      capability: Provider.add,
+      principal: Verifier,
+      authority: service,
+    })
+
+    assert.equal(result.error, true)
+  })
+
+  it('can not change with field', async () => {
+    const agent = alice
+    const account = 'did:mailto:mallory.com:mallory'
+    const space = mallory
+    const auth = Provider.add.invoke({
+      issuer: bob,
+      audience: service,
+      with: 'did:mailto:mallory.com:bob',
+      nb: {
+        provider: 'did:web:web3.storage:providers:w3up-alpha',
+        consumer: bob.did(),
+      },
+      proofs: [
+        await ucanto.delegate({
+          issuer: agent,
+          audience: bob,
+          capabilities: [
+            {
+              with: account,
+              can: 'provider/add',
+              nb: {
+                provider: 'did:web:web3.storage:providers:w3up-beta',
+                consumer: space.did(),
+              },
+            },
+          ],
+          proofs: await createAuthorization({ agent, service, account }),
+        }),
+      ],
+    })
+
+    const result = await access(await auth.delegate(), {
+      capability: Provider.add,
+      principal: Verifier,
+      authority: service,
+    })
+
+    assert.equal(result.error, true)
   })
 })
