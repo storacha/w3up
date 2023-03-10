8c6dea87a1d5e23e1195945f34eea547b60a9bda
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2023-03-10 12:36:10 -0800

    fix: DbProvisionsStorage putMany doesnt error on cid col conflict (#517)
    Motivation:
    * https://github.com/web3-storage/w3protocol/issues/482
    
    * [x] first draft
    
    After 2023-03-09 review with @Gozala :
    * [x] add test for updating Provision with same invocation cid but
    different derived
    fields. it should result in error
    * [x] make test pass, probably by changing the kysely to be 'on
    conflict, do nothing as long as the derived columns are identical,
    otherwise error'
    * it wasn't really possible to do that on conflict logic as far as I
    could tell, at least through our `kysely` orm
    * kysely doesn't expose methods for all the [`on conflict` clauses that
    sqlite should support](https://www.sqlite.org/lang_conflict.html)
    * I pursued something like [this using
    CTE](https://stackoverflow.com/a/49601238), only to find out [sqlite
    doesn't support `insert` in
    cte](https://sqlite.org/forum/forumpost/b44ca81f043bed9f)

--- packages/access-api/src/models/provisions.js
+++ packages/access-api/src/models/provisions.js
@@ -14,9 +14,9 @@ export function createProvisions(storage = []) {
     const hasRowWithSpace = storage.some(({ space }) => space === consumerId)
     return hasRowWithSpace
   }
-  /** @type {Provisions['putMany']} */
-  const putMany = async (...items) => {
-    storage.push(...items)
+  /** @type {Provisions['put']} */
+  const put = async (item) => {
+    storage.push(item)
   }
   /** @type {Provisions['count']} */
   const count = async () => {
@@ -24,13 +24,13 @@ export function createProvisions(storage = []) {
   }
   return {
     count,
-    putMany,
+    put,
     hasStorageProvider,
   }
 }
 
 /**
- * @typedef ProvsionsRow
+ * @typedef ProvisionsRow
  * @property {string} cid
  * @property {string} consumer
  * @property {string} provider
@@ -38,7 +38,7 @@ export function createProvisions(storage = []) {
  */
 
 /**
- * @typedef {import("../types/database").Database<{ provisions: ProvsionsRow }>} ProvisionsDatabase
+ * @typedef {import("../types/database").Database<{ provisions: ProvisionsRow }>} ProvisionsDatabase
  */
 
 /**
@@ -68,24 +68,68 @@ export class DbProvisions {
     return BigInt(size)
   }
 
-  /** @type {Provisions['putMany']} */
-  async putMany(...items) {
-    if (items.length === 0) {
+  /** @type {Provisions['put']} */
+  async put(item) {
+    /** @type {ProvisionsRow} */
+    const row = {
+      cid: item.invocation.cid.toString(),
+      consumer: item.space,
+      provider: item.provider,
+      sponsor: item.account,
+    }
+    /** @type {Array<keyof ProvisionsRow>} */
+    const rowColumns = ['cid', 'consumer', 'provider', 'sponsor']
+    const insert = this.#db
+      .insertInto(this.tableNames.provisions)
+      .values(row)
+      .returning(rowColumns)
+
+    let primaryKeyError
+    try {
+      await insert.executeTakeFirstOrThrow()
+    } catch (error) {
+      const d1Error = extractD1Error(error)
+      switch (d1Error?.code) {
+        case 'SQLITE_CONSTRAINT_PRIMARYKEY': {
+          primaryKeyError = error
+          break
+        }
+        default: {
+          throw error
+        }
+      }
+    }
+
+    if (!primaryKeyError) {
+      // no error inserting, we're done with put
       return
     }
-    /** @type {ProvsionsRow[]} */
-    const rows = items.map((item) => {
-      return {
-        cid: item.invocation.cid.toString(),
-        consumer: item.space,
-        provider: item.provider,
-        sponsor: item.account,
-      }
-    })
-    await this.#db
-      .insertInto(this.tableNames.provisions)
-      .values(rows)
+
+    // there was already a row with this invocation cid
+    // as long as the row we tried to insert is same as one already there, no need to error.
+    // so let's compare the existing row with that cid to the row we tried to insert.
+    const existing = await this.#db
+      .selectFrom(this.tableNames.provisions)
+      .select(rowColumns)
+      .where('cid', '=', row.cid)
       .executeTakeFirstOrThrow()
+    if (deepEqual(existing, row)) {
+      // the insert failed, but the existing row is identical to the row that failed to insert.
+      // so the put is a no-op, and we can consider it a success despite encountering the primaryKeyError
+      return
+    }
+
+    // this is a sign of something very wrong. throw so error reporters can report on it
+    // and determine what led to a put() with same invocation cid but new non-cid column values
+    throw Object.assign(
+      new Error(
+        `Provision with cid ${item.invocation.cid} already exists with different field values`
+      ),
+      {
+        insertion: row,
+        existing,
+      }
+    )
   }
 
   /** @type {Provisions['hasStorageProvider']} */
@@ -111,4 +155,36 @@ export class DbProvisions {
       .execute()
     return rows
   }
+}
+
+/**
+ * @param {Record<string,any>} x
+ * @param {Record<string,any>} y
+ * @returns {boolean}
+ */
+function deepEqual(x, y) {
+  const ok = Object.keys
+  const tx = typeof x
+  const ty = typeof y
+  return x && y && tx === 'object' && tx === ty
+    ? ok(x).length === ok(y).length &&
+        ok(x).every((key) => deepEqual(x[key], y[key]))
+    : x === y
+}
+
+/**
+ * @param {unknown} error
+ */
+function extractD1Error(error) {
+  const isD1 = /D1_ALL_ERROR/.test(String(error))
+  if (!isD1) return
+  const cause =
+    error && typeof error === 'object' && 'cause' in error && error.cause
+  const code =
+    cause &&
+    typeof cause === 'object' &&
+    'code' in cause &&
+    typeof cause.code === 'string' &&
+    cause.code
+  return { cause, code }
 }
