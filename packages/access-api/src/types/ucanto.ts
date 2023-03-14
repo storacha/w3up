8c63ccb31a0b67daeb486c5599664879a40763bb
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2023-03-13 18:28:25 -0700

    remove ts-ignore

--- packages/access-api/src/types/ucanto.ts
+++ packages/access-api/src/types/ucanto.ts
@@ -1,7 +1,8 @@
 import * as Ucanto from '@ucanto/interface'
 
-export type ServiceInvoke<Service extends Record<string, any>> = <
-  Capability extends Ucanto.Capability
->(
-  invocation: Ucanto.ServiceInvocation<Capability, Service>
+export type ServiceInvoke<
+  Service extends Record<string, any>,
+  InvocationCapabilities extends Ucanto.Capability = Ucanto.Capability
+> = <Capability extends InvocationCapabilities>(
+  invocation: Ucanto.ServiceInvocation<Capability>
 ) => Promise<Ucanto.InferServiceInvocationReturn<Capability, Service>>
