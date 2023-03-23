3d2ba4d6d4c9e9c181b79dbbfb016b50479cf4d1
Irakli Gozalishvili <contact@gozala.io>
2023-03-23 13:57:29 -0700

    chore: remove exp backoff on ucanlog

--- packages/access-api/src/utils/ucan-log.js
+++ packages/access-api/src/utils/ucan-log.js
@@ -30,25 +30,20 @@ class UCANLog {
    */
   async logInvocations(car) {
     try {
-      await pRetry(
-        async () => {
-          const res = await fetch(`${this.url}/ucan`, {
-            method: 'POST',
-            headers: {
-              Authorization: `Basic ${this.auth}`,
-              'Content-Type': 'application/invocations+car',
-            },
-            body: car,
-          })
-          if (!res.ok) {
-            throw new Error(`HTTP request status not ok: ${res.status}`)
-          }
-          return res
+      const res = await fetch(`${this.url}/ucan`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${this.auth}`,
+          'Content-Type': 'application/invocations+car',
         },
-        {
-          retries: 10,
-        }
-      )
+        body: car,
+      })
+      
+      if (!res.ok) {
+        throw new Error(`HTTP request status not ok: ${res.status}`)
+      }
+      
+      return res
     } catch (error) {
       throw new Error(`Failed to log invocations: ${error}`, { cause: error })
     }
@@ -75,7 +70,7 @@ class UCANLog {
           return res
         },
         {
-          retries: 10,
+          retries: 3,
         }
       )
     } catch (error) {
