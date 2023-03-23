fe26c7061c1f1e8da00411cd02420ab9d0f37094
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2023-03-22 22:28:17 -0700

    remove delegations_v2 schema and DbDelegationsStorage (keep newer DbDelegationsStorageWithR2)

--- packages/access-api/src/models/delegations.js
+++ packages/access-api/src/models/delegations.js
@@ -9,7 +9,7 @@ import {
  */
 
 /**
- * @template {import('../types/access-api-cf-db').DelegationsV2Row | import('../types/access-api-cf-db').DelegationsV3Row} DelegationRow
+ * @template {import('../types/access-api-cf-db').DelegationsV3Row} DelegationRow
  * @typedef {Omit<DelegationRow, 'inserted_at'|'updated_at'|'expires_at'>} DelegationRowUpdate
  */
 
@@ -21,146 +21,11 @@ import {
 export const delegationsV2TableName = /** @type {const} */ ('delegations_v2')
 
 /**
- * DelegationsStorage that persists using SQL.
- * * should work with cloudflare D1 v2 schema
- *
- * @deprecated - use AccessApiD1TablesV3 and DbDelegationsStorageWithR2
- *
- * @template {Database<import('../types/access-api-cf-db').DelegationsV2Tables>} DB
- */
-export class DbDelegationsStorage {
-  /** @type {DB} */
-  #db
-  #tables = {
-    delegations: delegationsV2TableName,
-  }
-
-  /**
-   * @param {DB} db
-   */
-  constructor(db) {
-    this.#db = db
-    // eslint-disable-next-line no-void
-    void (
-      /** @type {import('../types/delegations').DelegationsStorage} */ (this)
-    )
-  }
-
-  async count() {
-    return count(this.#db, this.#tables.delegations)
-  }
-
-  /**
-   * @param {import('../types/delegations').Query} query
-   */
-  async *find(query) {
-    const { audience } = query
-    const { delegations } = this.#tables
-    const selection = await this.#db
-      .selectFrom(delegations)
-      .selectAll()
-      .where(`${delegations}.audience`, '=', audience)
-      .execute()
-    for await (const row of selection) {
-      yield rowToDelegation(row)
-    }
-  }
-
-  /**
-   * store items
-   *
-   * @param  {Array<Ucanto.Delegation>} delegations
-   * @returns {Promise<void>}
-   */
-  async putMany(...delegations) {
-    if (delegations.length === 0) {
-      return
-    }
-    const values = delegations.map((d) => createDelegationRowUpdate(d))
-    await this.#db
-      .insertInto(this.#tables.delegations)
-      .values(values)
-      .onConflict((oc) => oc.column('cid').doNothing())
-      .executeTakeFirst()
-  }
-
-  /**
-   * iterate through all stored items
-   *
-   * @returns {AsyncIterableIterator<Ucanto.Delegation>}
-   */
-  async *[Symbol.asyncIterator]() {
-    if (!this.#db.canStream) {
-      throw Object.assign(
-        new Error(
-          `cannot create asyncIterator because the underlying database does not support streaming`
-        ),
-        { name: 'NotImplementedError' }
-      )
-    }
-    for await (const row of this.#db
-      .selectFrom(this.#tables.delegations)
-      .select(['bytes'])
-      .stream()) {
-      yield rowToDelegation(row)
-    }
-  }
-}
-
-/**
  * indicates that processing failed due to encountering an unexpected delegation.
  * e.g. if a delegation could not be parsed from underlying storage
  */
-class UnexpectedDelegation extends Error {
+export class UnexpectedDelegation extends Error {
   name = 'UnexpectedDelegation'
-}
-
-/**
- * @param {Pick<import('../types/access-api-cf-db').DelegationsV2Row, 'bytes'>} row
- * @returns {Ucanto.Delegation}
- */
-function rowToDelegation(row) {
-  /** @type {Ucanto.Delegation[]} */
-  let delegations = []
-  try {
-    delegations = bytesToDelegations(new Uint8Array(row.bytes))
-  } catch (error) {
-    if (
-      typeof error === 'object' &&
-      error &&
-      error.toString() === 'TypeError: Input should be a non-empty Uint8Array.'
-    ) {
-      throw Object.assign(
-        new UnexpectedDelegation(`failed to create delegation from row`, {
-          cause: error,
-        }),
-        // adding these so they appear in sentry et al and can aid debugging
-        {
-          row,
-        }
-      )
-    }
-    throw error
-  }
-  if (delegations.length !== 1) {
-    throw new Error(
-      `unexpected number of delegations from bytes: ${delegations.length}`
-    )
-  }
-  return delegations[0]
-}
-
-/**
- * @param {Ucanto.Delegation} d
- * @returns {DelegationRowUpdate<import('../types/access-api-cf-db').DelegationsV2Row>}
- */
-export function createDelegationRowUpdate(d) {
-  return {
-    cid: d.cid.toV1().toString(),
-    audience: d.audience.did(),
-    issuer: d.issuer.did(),
-    bytes: delegationsToBytes([d]),
-  }
 }
 
 /**
@@ -304,12 +169,11 @@ export class DbDelegationsStorageWithR2 {
 
 /**
  * @typedef {import('../types/access-api-cf-db').DelegationsV3Row} DelegationsV3Row
- * @typedef {import('../types/access-api-cf-db').DelegationsV2Row} DelegationsV2Row
  */
 
 /**
  * @template {string} TableName
- * @template {Record<TableName, DelegationsV3Row|DelegationsV2Row>} Tables
+ * @template {Record<TableName, DelegationsV3Row>} Tables
  * @param {import('../types/database').Database<Tables>} db
  * @param {TableName} delegationsTable
  * @returns {Promise<bigint>} - count of table
--- packages/access-api/src/types/access-api-cf-db.ts
+++ packages/access-api/src/types/access-api-cf-db.ts
@@ -1,32 +1,9 @@
-import { URI } from '@ucanto/interface'
 import { ColumnType, Generated } from 'kysely'
 
 export { R2Bucket } from '@miniflare/r2'
 
-// v2
-
-/**
- * @deprecated - use DelegationsV3Row
- */
-export interface DelegationsV2Row {
-  cid: string
-  bytes: Uint8Array | number[]
-  audience: URI<'did:'>
-  issuer: URI<'did:'>
-  expires_at: Date | null
-  inserted_at: Generated<Date>
-  updated_at: ColumnType<Date, never, Date>
-}
-
-/**
- * @deprecated - use DelegationsV3Tables
- */
-export interface DelegationsV2Tables {
-  delegations_v2: DelegationsV2Row
-}
-
 // v3
-// * drop 'bytes' column in favor of storing them in R2 (see DbDelegationsStorageWithR2)
+// * dropped 'bytes' column in favor of storing them in R2 (see DbDelegationsStorageWithR2)
 
 export interface DelegationsV3Row {
   cid: string
