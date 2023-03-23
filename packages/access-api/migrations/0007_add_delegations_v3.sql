098d0f94f9507217eb5d0fc5f233ab336fa7f1ae
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2023-03-22 23:45:05 -0700

    assert cid string kind in d1 cid column and r2 key

--- packages/access-api/migrations/0007_add_delegations_v3.sql
+++ packages/access-api/migrations/0007_add_delegations_v3.sql
@@ -9,6 +9,7 @@ context: we're going to start storing bytes outside of the database (e.g. in R2)
 
 CREATE TABLE
     IF NOT EXISTS delegations_v3 (
+        /* cidv1 dag-ucan/dag-cbor sha2-256 */
         cid TEXT NOT NULL PRIMARY KEY,
         audience TEXT NOT NULL,
         issuer TEXT NOT NULL,
