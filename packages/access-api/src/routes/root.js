d52a2813bf6123b71c93ffc47c3a5699f305612e
Irakli Gozalishvili <contact@gozala.io>
2023-03-23 07:40:01 -0700

    feat: write invocations and receipts into ucan log (#592)
    Fixes #581 
    
    ## Overview
    
    - Integrates changes from #579
      - I have added `undici` to [pnpm overrides](https://pnpm.io/package_json)
    - Gets rid of `raw` route in favor of `root` which now uses decoder/encoder based of content type
    - Defines `UCANLog` interface for sending invocations & receipts to kenisis
       - As per #579 we probably want to use CF Queue for failure tolerance which can be a followup improvement
    - Type fixes to unblock some changes
       - You can't do `@implements {import('./bindings').UCANLog}` nor you can type alias and do implements because it no longer an interface. Solution is to do `import * as API from "./bindings.js"` in which case you can `@implements {API.UCANLog}`, which is why end up changing `bindings.d.ts` to `bindings.ts` + `bindings.js`.
       - Turns out `.d.ts` files aren't type checked so ☝️ uncovered bunch of errors I had to fix
    
    ## Notes
    
    - I'm not happy how this reaches deep into ucanto & I'd like to uplift changes around encode / decode / execute into ucanto. Once that is done I'll make a followup change here to replace some of these with ucanto exposed functionality
       - It also would make it easier to integrate things in the upload-api
    - I do not seem to have privileges to add vars and secrets in this, so someone with enough privileges should add `UCAN_LOG_URL` or` UCAN_LOG_BASIC_AUTH` so someone needs to do it to make this work.
    - I end up sending invocations and receipts to the same endpoint but different content-type header which I think makes sense.
       - Also unlike #579 I've decided to decouple endpoint from upload endpoint as they're logically unrelated.

--- packages/access-api/src/routes/root.js
+++ packages/access-api/src/routes/root.js
@@ -2,12 +2,16 @@ import * as Server from '@ucanto/server'
 import * as CAR from '@ucanto/transport/car'
 import * as CBOR from '@ucanto/transport/cbor'
 import { service } from '../service/index.js'
+import * as API from '../bindings.js'
+import { serverCodec as json } from '../ucanto/server-codec.js'
 
 /**
  * @param {import('@web3-storage/worker-utils/router').ParsedRequest} request
- * @param {import('../bindings.js').RouteContext} env
+ * @param {API.RouteContext} env
+ * @param {API.HandlerContext} ctx
+ *
  */
-export async function postRoot(request, env) {
+export async function postRoot(request, env, ctx) {
   const server = Server.create({
     id: env.signer,
     encoder: CBOR,
@@ -18,10 +22,84 @@ export async function postRoot(request, env) {
     },
   })
 
-  const rsp = await server.request({
-    body: new Uint8Array(await request.arrayBuffer()),
+  const body = new Uint8Array(await request.arrayBuffer())
+  const headers = Object.fromEntries(request.headers.entries())
+
+  /** @type {[Server.RequestDecoder, Server.ResponseEncoder]} */
+  // We will use encoder / decoder pair based on the content-type header this
+  // way we do not need to have separate routes.
+  const [decoder, encoder] =
+    headers['content-type'] === 'application/car' ? [CAR, CBOR] : [json, json]
+
+  const invocations = await decoder.decode({
+    body,
     headers: Object.fromEntries(request.headers.entries()),
   })
 
-  return new Response(rsp.body, { headers: rsp.headers })
+  // We block until we can log the UCAN invocation if this fails we return a 500
+  // to the client. That is because in the future we expect that invocations will
+  // be written to a queue first and then processed asynchronously, so if we
+  // fail to queue the invocation we should not handle it.
+  await env.ucanLog.logInvocations(body)
+
+  const results = await Promise.all(
+    invocations.map((invocation) => execute(invocation, server, env))
+  )
+
+  const forks = []
+  const out = []
+
+  for (const receipt of results) {
+    out.push(receipt.data.out.error || receipt.data.out.ok)
+    // we don't await the logReceipt call because we want to return the response
+    // to the client as soon as possible. We will however keep the worker alive
+    // until we have logged the receipt. If logging fails we will log the error
+    // as we have no way to handle it here nor we can rollback the invocation.
+    forks.push(
+      env.ucanLog.logReceipt(receipt).catch((error) => env.log.error(error))
+    )
+  }
+
+  const response = await encoder.encode(out)
+
+  ctx.waitUntil(Promise.all(forks))
+
+  return new Response(response.body, {
+    headers: response.headers,
+  })
+}
+
+/**
+ *
+ * @param {Server.Invocation} invocation
+ * @param {Server.ServerView<*>} server
+ * @param {API.RouteContext} env
+ * @returns {Promise<Required<API.Block<API.Receipt>>>}
+ */
+const execute = async (invocation, server, env) => {
+  /** @type {[Server.Result<*, Server.API.Failure>]} */
+  const [result] = await Server.execute([invocation], server)
+  const out = result?.error ? { error: result } : { ok: result }
+
+  // Create a receipt payload for the invocation conforming to the spec
+  // @see https://github.com/ucan-wg/invocation/#8-receipt
+  const payload = {
+    ran: invocation.cid,
+    out,
+    fx: { fork: [] },
+    meta: {},
+    iss: env.signer.did(),
+    prf: [],
+  }
+
+  // create a receipt by signing the payload with a server key
+  const receipt = {
+    ...payload,
+    s: await env.signer.sign(CBOR.codec.encode(payload)),
+  }
+
+  return {
+    data: receipt,
+    ...(await CBOR.codec.write(receipt)),
+  }
 }
