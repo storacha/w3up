af0617f8103f175712dad6dce162fd2c93c40d22
Alan Shaw <alan.shaw@protocol.ai>
2023-03-29 14:17:35 +0100

    feat: bring your own principal

--- packages/w3up-client/test/index.node.test.js
+++ packages/w3up-client/test/index.node.test.js
@@ -1,4 +1,5 @@
 import assert from 'assert'
+import { Signer } from '@ucanto/principal/ed25519'
 import { EdDSA } from '@ipld/dag-ucan/signature'
 import { StoreConf } from '@web3-storage/access/stores/store-conf'
 import { create } from '../src/index.node.js'
@@ -19,5 +20,29 @@ describe('create', () => {
     const client1 = await create({ store })
 
     assert.equal(client0.agent().did(), client1.agent().did())
+  })
+
+  it('should allow BYO principal', async () => {
+    const store = new StoreConf({ profile: 'w3up-client-test' })
+    await store.reset()
+
+    const principal = await Signer.generate()
+    const client = await create({ principal, store })
+
+    assert.equal(client.agent().did(), principal.did())
+  })
+
+  it('should throw for mismatched BYO principal', async () => {
+    const store = new StoreConf({ profile: 'w3up-client-test' })
+    await store.reset()
+
+    const principal0 = await Signer.generate()
+    await create({ principal: principal0, store })
+
+    const principal1 = await Signer.generate()
+    await assert.rejects(
+      create({ principal: principal1, store }),
+      { message: `store cannot be used with ${principal1.did()}, stored principal and passed principal must match` }
+    )
   })
 })
