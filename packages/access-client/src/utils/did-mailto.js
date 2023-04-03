9e49332b13141dad65ffcf2af6fa56a557410dc1
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2023-04-03 16:50:04 -0700

    access-client deps on did-mailto package

--- packages/access-client/src/utils/did-mailto.js
+++ packages/access-client/src/utils/did-mailto.js
@@ -1,15 +1,9 @@
+import * as DidMailto from '@web3-storage/did-mailto'
+
 /**
  * @param {string} email
  * @returns {`did:mailto:${string}:${string}`}
  */
 export function createDidMailtoFromEmail(email) {
-  const emailParts = email.split('@')
-  if (emailParts.length !== 2) {
-    throw new Error(`unexpected email ${email}`)
-  }
-  const [local, domain] = emailParts
-  const did = /** @type {const} */ (
-    `did:mailto:${encodeURIComponent(domain)}:${encodeURIComponent(local)}`
-  )
-  return did
+  return DidMailto.fromEmail(DidMailto.email(email))
 }
