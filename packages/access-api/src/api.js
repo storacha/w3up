b23eb52d300aae0b28b0835d4b8ecfaf98fd8027
Irakli Gozalishvili <contact@gozala.io>
2023-05-01 21:57:01 -0700

    can't offend eslint by lack of line break

--- packages/access-api/src/api.js
+++ packages/access-api/src/api.js
@@ -1,6 +1,7 @@
 /**
  * This is file is shadowed by api.ts and only exists so JS could import it to
  * refer to types without encountering runtime errors
+ *
  * @see https://github.com/microsoft/TypeScript/issues/41825
  */
 
