944182ae97c5e69899d6a307cd73683852fb89dd
Alan Shaw <alan.shaw@protocol.ai>
2023-09-07 10:18:53 +0100

    feat: adjust shard size (#908)
    As discussed in #764 and calculated in
    https://observablehq.com/@gozala/w3up-shard-size, this PR adjusts the
    `upload-client` to shard on a CAR size of <= `133,169,152` bytes.
    
    It also fixes a bug where a CAR shard could be created with size greater
    than the shard size.
    
    supersedes #764
    
    ---------
    
    Co-authored-by: Irakli Gozalishvili <contact@gozala.io>

--- packages/upload-client/src/sharding.js
+++ packages/upload-client/src/sharding.js
@@ -1,8 +1,9 @@
 import Queue from 'p-queue'
-import { encode } from './car.js'
+import { blockEncodingLength, encode, headerEncodingLength } from './car.js'
 import { add } from './store.js'
 
-const SHARD_SIZE = 1024 * 1024 * 100
+// https://observablehq.com/@gozala/w3up-shard-size
+const SHARD_SIZE = 133_169_152
 const CONCURRENT_UPLOADS = 3
 
 /**
@@ -18,37 +19,69 @@ export class ShardingStream extends TransformStream {
    */
   constructor(options = {}) {
     const shardSize = options.shardSize ?? SHARD_SIZE
+    const maxBlockLength = shardSize - headerEncodingLength()
     /** @type {import('@ipld/unixfs').Block[]} */
-    let shard = []
+    let blocks = []
     /** @type {import('@ipld/unixfs').Block[] | null} */
-    let readyShard = null
-    let size = 0
+    let readyBlocks = null
+    let currentLength = 0
 
     super({
       async transform(block, controller) {
-        if (readyShard != null) {
-          controller.enqueue(await encode(readyShard))
-          readyShard = null
+        if (readyBlocks != null) {
+          controller.enqueue(await encode(readyBlocks))
+          readyBlocks = null
         }
-        if (shard.length && size + block.bytes.length > shardSize) {
-          readyShard = shard
-          shard = []
-          size = 0
+
+        const blockLength = blockEncodingLength(block)
+        if (blockLength > maxBlockLength) {
+          throw new Error(
+            `block will cause CAR to exceed shard size: ${block.cid}`
+          )
         }
-        shard.push(block)
-        size += block.bytes.length
+
+        if (blocks.length && currentLength + blockLength > maxBlockLength) {
+          readyBlocks = blocks
+          blocks = []
+          currentLength = 0
+        }
+        blocks.push(block)
+        currentLength += blockLength
       },
 
       async flush(controller) {
-        if (readyShard != null) {
-          controller.enqueue(await encode(readyShard))
+        if (readyBlocks != null) {
+          controller.enqueue(await encode(readyBlocks))
         }
 
-        const rootBlock = shard.at(-1)
-        if (rootBlock != null) {
-          controller.enqueue(
-            await encode(shard, options.rootCID ?? rootBlock.cid)
-          )
+        const rootBlock = blocks.at(-1)
+        if (rootBlock == null) return
+
+        const rootCID = options.rootCID ?? rootBlock.cid
+        const headerLength = headerEncodingLength(rootCID)
+
+        // if adding CAR root overflows the shard limit we move overflowing
+        // blocks into a another CAR.
+        if (headerLength + currentLength > shardSize) {
+          const overage = headerLength + currentLength - shardSize
+          const overflowBlocks = []
+          let overflowCurrentLength = 0
+          while (overflowCurrentLength < overage) {
+            const block = blocks[blocks.length - 1]
+            blocks.pop()
+            overflowBlocks.unshift(block)
+            overflowCurrentLength += blockEncodingLength(block)
+
+            // need at least 1 block in original shard
+            if (blocks.length < 1)
+              throw new Error(
+                `block will cause CAR to exceed shard size: ${block.cid}`
+              )
+          }
+          controller.enqueue(await encode(blocks))
+          controller.enqueue(await encode(overflowBlocks, rootCID))
+        } else {
+          controller.enqueue(await encode(blocks, rootCID))
         }
       },
     })
