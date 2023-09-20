20f80af29871579c0c2693fb38101e3e8528e34d
Alan Shaw <alan.shaw@protocol.ai>
2023-09-20 17:47:07 +0100

    refactor!: filecoin client to use new capabilities
    docs: better docs
    
    fix: client tests

--- packages/filecoin-client/test/aggregator.test.js
+++ packages/filecoin-client/test/aggregator.test.js
@@ -3,42 +3,36 @@ import * as Signer from '@ucanto/principal/ed25519'
 import * as Client from '@ucanto/client'
 import * as Server from '@ucanto/server'
 import * as CAR from '@ucanto/transport/car'
-import { Filecoin as FilecoinCapabilities } from '@web3-storage/capabilities'
-
-import { aggregateQueue, aggregateAdd } from '../src/aggregator.js'
-
-import { randomCargo } from './helpers/random.js'
+import * as AggregatorCaps from '@web3-storage/capabilities/filecoin/aggregator'
+import { pieceOffer, pieceAccept } from '../src/aggregator.js'
+import { randomAggregate, randomCargo } from './helpers/random.js'
 import { mockService } from './helpers/mocks.js'
-import { OperationFailed, OperationErrorName } from './helpers/errors.js'
 import { validateAuthorization } from './helpers/utils.js'
 import { serviceProvider as aggregatorService } from './fixtures.js'
 
-describe('aggregate/add', () => {
-  it('storefront queues a filecoin piece for aggregator to handle', async () => {
+describe('aggregator', () => {
+  it('storefront offers a filecoin piece', async () => {
     const { storefront } = await getContext()
 
     // Generate cargo to add
     const [cargo] = await randomCargo(1, 100)
     const group = 'did:web:free.web3.storage'
 
-    /** @type {import('@web3-storage/capabilities/types').AggregateAddSuccess} */
-    const pieceAddResponse = {
+    /** @type {import('@web3-storage/capabilities/types').PieceOfferSuccess} */
+    const pieceOfferResponse = {
       piece: cargo.link,
     }
 
     // Create Ucanto service
     const service = mockService({
-      aggregate: {
-        queue: Server.provideAdvanced({
-          capability: FilecoinCapabilities.aggregateQueue,
+      piece: {
+        offer: Server.provideAdvanced({
+          capability: AggregatorCaps.pieceOffer,
           handler: async ({ invocation, context }) => {
             assert.strictEqual(invocation.issuer.did(), storefront.did())
             assert.strictEqual(invocation.capabilities.length, 1)
             const invCap = invocation.capabilities[0]
-            assert.strictEqual(
-              invCap.can,
-              FilecoinCapabilities.aggregateQueue.can
-            )
+            assert.strictEqual(invCap.can, AggregatorCaps.pieceOffer.can)
             assert.equal(invCap.with, invocation.issuer.did())
             // piece link
             assert.ok(invCap.nb?.piece.equals(cargo.link.link()))
@@ -46,30 +40,25 @@ describe('aggregate/add', () => {
             assert.strictEqual(invCap.nb?.group, group)
 
             // Create effect for receipt with self signed queued operation
-            const fx = await FilecoinCapabilities.aggregateAdd
+            const fx = await AggregatorCaps.pieceAccept
               .invoke({
                 issuer: context.id,
                 audience: context.id,
                 with: context.id.did(),
                 nb: {
                   ...invCap.nb,
-                  // add storefront
-                  storefront: invCap.with,
                 },
               })
               .delegate()
 
-            return Server.ok(pieceAddResponse).join(fx.link())
+            return Server.ok(pieceOfferResponse).join(fx.link())
           },
         }),
-        add: () => {
-          throw new Error('not implemented')
-        },
       },
     })
 
-    // invoke piece add from storefront
-    const res = await aggregateQueue(
+    // invoke piece offer from storefront
+    const res = await pieceOffer(
       {
         issuer: storefront,
         with: storefront.did(),
@@ -81,130 +70,69 @@ describe('aggregate/add', () => {
     )
 
     assert.ok(res.out.ok)
-    assert.ok(res.out.ok.piece.equals(pieceAddResponse.piece))
+    assert.ok(res.out.ok.piece.equals(pieceOfferResponse.piece))
     // includes effect fx in receipt
     assert.ok(res.fx.join)
   })
 
-  it('aggregator self invokes add a filecoin piece to accept the piece queued', async () => {
-    const { storefront } = await getContext()
-
-    // Generate cargo to add
-    const [cargo] = await randomCargo(1, 100)
-    const storefrontId = storefront.did()
+  it('aggregator accepts a filecoin piece', async () => {
+    const { pieces, aggregate } = await randomAggregate(100, 100)
     const group = 'did:web:free.web3.storage'
 
-    /** @type {import('@web3-storage/capabilities/types').AggregateAddSuccess} */
-    const pieceAddResponse = {
-      piece: cargo.link,
+    /** @type {import('@web3-storage/capabilities/types').PieceAcceptSuccess} */
+    const pieceAcceptResponse = {
+      piece: pieces[0].link,
+      aggregate: aggregate.link,
+      inclusion: {
+        subtree: {
+          path: [],
+          index: 0n,
+        },
+        index: {
+          path: [],
+          index: 0n,
+        },
+      },
     }
 
     // Create Ucanto service
     const service = mockService({
-      aggregate: {
-        add: Server.provideAdvanced({
-          capability: FilecoinCapabilities.aggregateAdd,
+      piece: {
+        accept: Server.provideAdvanced({
+          capability: AggregatorCaps.pieceAccept,
           handler: async ({ invocation }) => {
             assert.strictEqual(invocation.issuer.did(), aggregatorService.did())
             assert.strictEqual(invocation.capabilities.length, 1)
             const invCap = invocation.capabilities[0]
-            assert.strictEqual(
-              invCap.can,
-              FilecoinCapabilities.aggregateAdd.can
-            )
+            assert.strictEqual(invCap.can, AggregatorCaps.pieceAccept.can)
             assert.equal(invCap.with, invocation.issuer.did())
             // piece link
-            assert.ok(invCap.nb?.piece.equals(cargo.link.link()))
+            assert.ok(invCap.nb?.piece.equals(pieces[0].link))
             // group
             assert.strictEqual(invCap.nb?.group, group)
 
-            return Server.ok(pieceAddResponse)
+            return Server.ok(pieceAcceptResponse)
           },
         }),
-        queue: () => {
-          throw new Error('not implemented')
-        },
       },
     })
 
-    // self invoke piece/add from aggregator
-    const res = await aggregateAdd(
+    // self invoke piece/offer from aggregator
+    const res = await pieceAccept(
       {
         issuer: aggregatorService,
         with: aggregatorService.did(),
         audience: aggregatorService,
       },
-      cargo.link.link(),
-      storefrontId,
+      pieces[0].link,
       group,
       { connection: getConnection(service).connection }
     )
 
     assert.ok(res.out.ok)
-    assert.ok(res.out.ok.piece.equals(pieceAddResponse.piece))
-    // does not include effect fx in receipt
-    assert.ok(!res.fx.join)
-  })
-
-  it('aggregator self invokes add a filecoin piece to reject the piece queued', async () => {
-    const { storefront } = await getContext()
-
-    // Generate cargo to add
-    const [cargo] = await randomCargo(1, 100)
-    const storefrontId = storefront.did()
-    const group = 'did:web:free.web3.storage'
-
-    /** @type {import('@web3-storage/capabilities/types').AggregateAddFailure} */
-    const pieceAddResponse = new OperationFailed(
-      'failed to add to aggregate',
-      cargo.link
-    )
-
-    // Create Ucanto service
-    const service = mockService({
-      aggregate: {
-        add: Server.provideAdvanced({
-          capability: FilecoinCapabilities.aggregateAdd,
-          handler: async ({ invocation }) => {
-            assert.strictEqual(invocation.issuer.did(), aggregatorService.did())
-            assert.strictEqual(invocation.capabilities.length, 1)
-            const invCap = invocation.capabilities[0]
-            assert.strictEqual(
-              invCap.can,
-              FilecoinCapabilities.aggregateAdd.can
-            )
-            assert.equal(invCap.with, invocation.issuer.did())
-            // piece link
-            assert.ok(invCap.nb?.piece.equals(cargo.link.link()))
-            // group
-            assert.strictEqual(invCap.nb?.group, group)
-
-            return {
-              error: pieceAddResponse,
-            }
-          },
-        }),
-        queue: () => {
-          throw new Error('not implemented')
-        },
-      },
-    })
-
-    // self invoke piece add from aggregator
-    const res = await aggregateAdd(
-      {
-        issuer: aggregatorService,
-        with: aggregatorService.did(),
-        audience: aggregatorService,
-      },
-      cargo.link.link(),
-      storefrontId,
-      group,
-      { connection: getConnection(service).connection }
-    )
-
-    assert.ok(res.out.error)
-    assert.deepEqual(res.out.error.name, OperationErrorName)
+    assert.ok(res.out.ok.piece.equals(pieceAcceptResponse.piece))
+    assert.ok(res.out.ok.aggregate.equals(pieceAcceptResponse.aggregate))
+    assert.deepEqual(res.out.ok.inclusion, pieceAcceptResponse.inclusion)
     // does not include effect fx in receipt
     assert.ok(!res.fx.join)
   })
