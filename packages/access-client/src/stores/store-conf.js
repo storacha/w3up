7a95162f3a00b936451d8419bcf8dd34375b0a83
bengo <171782+gobengo@users.noreply.github.com>
2023-10-24 11:24:05 -0700

    packages/access-client uses tsconfig module/moduleResolution of NodeNext to fix attw errors

--- packages/access-client/src/stores/store-conf.js
+++ packages/access-client/src/stores/store-conf.js
@@ -9,6 +9,6 @@ import { ConfDriver } from '../drivers/conf.js'
  * import { StoreConf } from '@web3-storage/access/stores/store-conf'
  * ```
  *
- * @extends {ConfDriver<import('../types').AgentDataExport>}
+ * @extends {ConfDriver<import('../types.js').AgentDataExport>}
  */
 export class StoreConf extends ConfDriver {}
