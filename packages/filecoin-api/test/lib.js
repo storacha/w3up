e442f32ce09d1457bc1df6c96ad006ec082d428d
Vasco Santos <santos.vasco10@gmail.com>
2023-10-24 14:59:12 +0200

    fix: export tests in lib for events (#1001)

--- packages/filecoin-api/test/lib.js
+++ packages/filecoin-api/test/lib.js
@@ -1,14 +1,38 @@
+import * as StorefrontService from './services/storefront.js'
 import * as AggregatorService from './services/aggregator.js'
 import * as DealerService from './services/dealer.js'
-import * as StorefrontService from './services/storefront.js'
+import * as DealTrackerService from './services/deal-tracker.js'
+
+import * as StorefrontEvents from './events/storefront.js'
+import * as AggregatorEvents from './events/aggregator.js'
+import * as DealerEvents from './events/dealer.js'
 
 export * from './utils.js'
 
 export const test = {
   service: {
-    ...AggregatorService.test,
-    ...DealerService.test,
-    ...StorefrontService.test,
+    storefront: {
+      ...StorefrontService.test,
+    },
+    aggregator: {
+      ...AggregatorService.test
+    },
+    dealer: {
+      ...DealerService.test,
+    },
+    dealTracker: {
+      ...DealTrackerService.test
+    }
   },
-  events: {},
+  events: {
+    storefront: {
+      ...StorefrontEvents.test,
+    },
+    aggregator: {
+      ...AggregatorEvents.test
+    },
+    dealer: {
+      ...DealerEvents.test,
+    },
+  },
 }
