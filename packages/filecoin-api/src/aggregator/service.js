90a5a4d815cff19d9421811a78dbefa01d486ebf
Vasco Santos <santos.vasco10@gmail.com>
2023-11-03 13:07:31 +0100

    fix: aggregate offer invocation cid wrong (#1063)
    because not pieces cid block being used but buffer block, which was
    meaning a different CID being computed for invocation and receipt, as
    seen in:
    
    -
    https://github.com/web3-storage/w3up/blob/main/packages/filecoin-client/src/dealer.js#L58
    -
    https://github.com/web3-storage/w3up/blob/main/packages/filecoin-api/src/aggregator/service.js#L97
    
    So, `AggregatorRecord` now has a property for buffer and one for pieces.
    `buffer` allows us to get to know the pieces inside the `buffer` while
    `pieces` is the direct encoding of `PieceLink[]` dag-cbor. We could just
    rely on the buffer, but for issuing all `aggregate/accept` receipts for
    each piece, we would need an extra S3 get to read the buffer and compute
    the dag cbor for the list of pieces inside the buffer

--- packages/filecoin-api/src/aggregator/service.js
+++ packages/filecoin-api/src/aggregator/service.js
@@ -77,6 +77,7 @@ export const pieceAccept = async ({ capability }, context) => {
     }
   }
 
+  // Get buffered pieces 
   const [{ aggregate, inclusion }] = getInclusionRes.ok
   const getAggregateRes = await context.aggregateStore.get({ aggregate })
   if (getAggregateRes.error) {
