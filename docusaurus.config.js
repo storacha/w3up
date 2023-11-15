d8ebc337d7b49ac42712e28b60934acb55cf438d
bengo <171782+gobengo@users.noreply.github.com>
2023-11-15 13:02:30 -0800

    finish fixing docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
         tsconfig: './tsconfig.json',
         out: 'markdown',
         sidebar: {
-          categoryLabel: 'w3protocol',
+          categoryLabel: 'w3up',
         },
       },
     ],
