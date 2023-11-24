bf9c78af404ea3f03f610789a92103a8e2eaf392
Alan Shaw <alan.shaw@protocol.ai>
2023-11-24 14:59:13 +0000

    feat: add store.get and upload.get to clients

--- packages/upload-client/src/upload.js
+++ packages/upload-client/src/upload.js
@@ -65,6 +65,63 @@ export async function add(
 }
 
 /**
+ * Get details of an "upload".
+ *
+ * Required delegated capability proofs: `upload/get`
+ *
+ * @param {import('./types.js').InvocationConfig} conf Configuration
+ * for the UCAN invocation. An object with `issuer`, `with` and `proofs`.
+ *
+ * The `issuer` is the signing authority that is issuing the UCAN
+ * invocation(s). It is typically the user _agent_.
+ *
+ * The `with` is the resource the invocation applies to. It is typically the
+ * DID of a space.
+ *
+ * The `proofs` are a set of capability delegations that prove the issuer
+ * has the capability to perform the action.
+ *
+ * The issuer needs the `upload/get` delegated capability.
+ * @param {import('multiformats/link').UnknownLink} root Root data CID for the DAG that was stored.
+ * @param {import('./types.js').RequestOptions} [options]
+ * @returns {Promise<import('./types.js').UploadGetSuccess>}
+ */
+export async function get(
+  { issuer, with: resource, proofs, audience },
+  root,
+  options = {}
+) {
+  /* c8 ignore next */
+  const conn = options.connection ?? connection
+  const result = await retry(
+    async () => {
+      return await UploadCapabilities.get
+        .invoke({
+          issuer,
+          /* c8 ignore next */
+          audience: audience ?? servicePrincipal,
+          with: SpaceDID.from(resource),
+          nb: { root },
+          proofs,
+        })
+        .execute(conn)
+    },
+    {
+      onFailedAttempt: console.warn,
+      retries: options.retries ?? REQUEST_RETRIES,
+    }
+  )
+
+  if (!result.out.ok) {
+    throw new Error(`failed ${UploadCapabilities.get.can} invocation`, {
+      cause: result.out.error,
+    })
+  }
+
+  return result.out.ok
+}
+
+/**
  * List uploads created by the issuer.
  *
  * @param {import('./types.js').InvocationConfig} conf Configuration
