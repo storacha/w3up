f31e50b34cd19500c187b5d6fcb0bdacb6a3eb1d
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2023-11-28 14:38:21 -0800

    add to eslint-config-w3up 'plugin:@typescript-eslint/recommended-type-checked'
    Motivation:
    * i want https://typescript-eslint.io/rules/no-floating-promises/
    * it would have caught a floating promise on `addSpace` that alan just reported

--- packages/eslint-config-w3up/index.js
+++ packages/eslint-config-w3up/index.js
@@ -4,6 +4,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:jsdoc/recommended',
   ],
   rules: {
