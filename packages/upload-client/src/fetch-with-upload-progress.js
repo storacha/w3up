6d78127430a49167675d96631be2db0ce820c73d
bengo <171782+gobengo@users.noreply.github.com>
2023-12-13 10:20:11 -0800

    rm unused logic

--- packages/upload-client/src/fetch-with-upload-progress.js
+++ packages/upload-client/src/fetch-with-upload-progress.js
@@ -4,8 +4,5 @@ import ipfsUtilsFetch from 'ipfs-utils/src/http/fetch.js'
  * @type {import('./types.js').FetchWithUploadProgress}
  */
 export const fetchWithUploadProgress = (url, init) => {
-  if (init && 'readable' in init) {
-    throw new Error('init cannot be readable')
-  }
   return ipfsUtilsFetch.fetch(url, init)
 }
