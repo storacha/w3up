8924290133f0913f64b2068a1395623c7321eb64
Travis Vachon <travis.vachon@gmail.com>
2024-01-08 14:40:05 -0800

    feat: introduce capability for changing billing plan
    We'd like to allow users to update their billing plans from console - introduce a new capability to enable this.

--- packages/capabilities/src/plan.js
+++ packages/capabilities/src/plan.js
@@ -1,5 +1,5 @@
-import { capability, ok } from '@ucanto/validator'
-import { AccountDID, equalWith, and } from './utils.js'
+import { DID, capability, ok, struct } from '@ucanto/validator'
+import { AccountDID, equal, equalWith, and } from './utils.js'
 
 /**
  * Capability can be invoked by an account to get information about
@@ -10,5 +10,23 @@ export const get = capability({
   with: AccountDID,
   derives: (child, parent) => {
     return and(equalWith(child, parent)) || ok({})
+  },
+})
+
+/**
+ * Capability can be invoked by an account to change its billing plan.
+ */
+export const update = capability({
+  can: 'plan/update',
+  with: AccountDID,
+  nb: struct({
+    product: DID,
+  }),
+  derives: (child, parent) => {
+    return (
+      and(equalWith(child, parent)) ||
+      and(equal(child.nb.product, parent.nb.product, 'product')) ||
+      ok({})
+    )
   },
 })
