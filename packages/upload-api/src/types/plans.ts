4a26291404993f5b746770c528b931dde348f8b4
Travis Vachon <travis.vachon@gmail.com>
2024-01-11 16:20:04 -0800

    wip: introduce `update` method on `PlansStorage`
    We already have a `set` method, but that is used to update our system when we get a webhook from Stripe. I debated adding an option to that or adding a new method to the interface specifically for a user of our system asking to update their plan.
    
    Putting this PR up now to get feedback and thoughts - does anyone feel strongly that we should do this a different way?

--- packages/upload-api/src/types/plans.ts
+++ packages/upload-api/src/types/plans.ts
@@ -1,5 +1,5 @@
 import * as Ucanto from '@ucanto/interface'
-import { AccountDID, DID, PlanGetFailure, PlanGetSuccess } from '../types.js'
+import { AccountDID, DID, PlanGetFailure, PlanGetSuccess, PlanUpdateFailure, PlanUpdateSuccess } from '../types.js'
 
 export type PlanID = DID
 
@@ -17,12 +17,30 @@ export interface PlansStorage {
   ) => Promise<Ucanto.Result<PlanGetSuccess, PlanGetFailure>>
 
   /**
-   * Set an account's plan
+   * Set an account's plan in our systems.
+   * 
+   * This should only update our systems, ie in a case where we have been notified
+   * that a user's plan has been updated in the third party billing system.
    *
    * @param account account DID
+   * @param plan the DID of the new plan
    */
   set: (
     account: AccountDID,
     plan: DID
   ) => Promise<Ucanto.Result<Ucanto.Unit, Ucanto.Failure>>
+
+  /**
+   * Update an account's plan in the billing system.
+   * 
+   * This should be used when a user manually updates their plan, and should
+   * update the plan value in both our systems and any third party billing systems.
+   *
+   * @param account account DID
+   * @param plan the DID of the new plan
+   */
+  update: (
+    account: AccountDID,
+    plan: DID
+  ) => Promise<Ucanto.Result<PlanUpdateSuccess, PlanUpdateFailure>>
 }
