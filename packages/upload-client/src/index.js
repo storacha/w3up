5329ce227f78b77f7c8aceb6003712b1599f71ea
Vasco Santos <santos.vasco10@gmail.com>
2024-03-21 12:19:48 +0100

    chore: address review comments
    Co-authored-by: Alan Shaw <alan.shaw@protocol.ai>

--- packages/upload-client/src/index.js
+++ packages/upload-client/src/index.js
@@ -148,7 +148,7 @@ async function uploadBlockStream(conf, blocks, options = {}) {
           )
 
           if (result.out.error) {
-            throw result.out.error
+            throw new Error('failed to offer piece for aggregation into filecoin deal', { cause: result.out.error })
           }
 
           const { version, roots, size } = car
