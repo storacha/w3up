1e4907f431cf6042e71e900dcdb938768929bf30
Alan Shaw <alan.shaw@protocol.ai>
2024-05-15 18:54:45 +0100

    chore: fml i actually stull need this

--- packages/blob-index/test/helpers/random.js
+++ packages/blob-index/test/helpers/random.js
@@ -3,13 +3,14 @@ import { CID } from 'multiformats'
 import { sha256 } from 'multiformats/hashes/sha2'
 import * as CAR from '@ucanto/transport/car'
 import * as raw from 'multiformats/codecs/raw'
+import { webcrypto } from 'one-webcrypto'
 
 /** @param {number} size */
 export async function randomBytes(size) {
   const bytes = new Uint8Array(size)
   while (size) {
     const chunk = new Uint8Array(Math.min(size, 65_536))
-    crypto.getRandomValues(chunk)
+    webcrypto.getRandomValues(chunk)
 
     size -= chunk.length
     bytes.set(chunk, size)
