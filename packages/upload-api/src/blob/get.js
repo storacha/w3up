08c169511619ad76533a53aca594d1b3022666f3
Alan Shaw <alan.shaw@protocol.ai>
2024-06-06 13:35:38 +0100

    fix: blob get

--- packages/upload-api/src/blob/get.js
+++ packages/upload-api/src/blob/get.js
@@ -1,8 +1,8 @@
 import * as Server from '@ucanto/server'
 import * as Blob from '@web3-storage/capabilities/blob'
+import * as Digest from 'multiformats/hashes/digest'
 import * as API from '../types.js'
 import { BlobNotFound } from './lib.js'
-import { decode } from 'multiformats/hashes/digest'
 
 /**
  * @param {API.BlobServiceContext} context
@@ -10,11 +10,11 @@ import { decode } from 'multiformats/hashes/digest'
  */
 export function blobGetProvider(context) {
   return Server.provide(Blob.get, async ({ capability }) => {
-    const { digest } = capability.nb
+    const digest = Digest.decode(capability.nb.digest)
     const space = Server.DID.parse(capability.with).did()
     const res = await context.allocationsStorage.get(space, digest)
     if (res.error && res.error.name === 'RecordNotFound') {
-      return Server.error(new BlobNotFound(decode(digest)))
+      return Server.error(new BlobNotFound(digest))
     }
     return res
   })
