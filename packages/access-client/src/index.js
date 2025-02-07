cf3eab9e81ba320b2c14f222524d14bc4b394a1d
Petra Jaros <peeja@peeja.com>
2025-02-07 16:07:18 -0500

    Remove Docusaurus & Typedoc

--- packages/access-client/src/index.js
+++ packages/access-client/src/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable jsdoc/check-tag-names */
 export * from './agent.js'
 
-// Workaround for typedoc until 0.24 support export maps
 export * as Encoding from './encoding.js'
 export { StoreConf } from './stores/store-conf.js'
 export { StoreIndexedDB } from './stores/store-indexeddb.js'
